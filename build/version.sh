21bdf279b1106ba4ba1c27d02721b03de701ea45
sbprolog <sbprolog>
1998-11-09 03:12:53 +0000

    Changed date, added "configure" to cvsignore

--- build/version.sh
+++ build/version.sh
@@ -7,4 +7,4 @@ xsb_codename="Code Breaker"
 
 # Format: YYYY-MM-DD or YYYY.MM.DD or YYYY/MM/DD
 # With this, we should become Y2K compliant :-)
-xsb_release_date=1998-11-02
+xsb_release_date=1998-11-08
