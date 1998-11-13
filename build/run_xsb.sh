cab1f4ffb0d5f2d37029f4e6b2a45a8d1f9a30d6
kifer <kifer>
1998-11-13 06:53:03 +0000

    aRenamed run_xsb into run_xsb.sh Updated configure

---
+++ build/run_xsb.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# This runs XSB on some packages, so they will be compiled at the
+# installation time. Gets the full path name of the xsb executable as the
+# first argument.
+
+xsb_executable=$1
+
+echo ""
+echo "Configuring XSB packages. Wait..."
+echo "[perlmatch]. [slx]. [flip]. run_all_demos, end. halt." \
+		    | $xsb_executable 
