c44d5b2a655a89fcaf00ed232a01b7798c7917ac
kifer <kifer>
1998-11-14 23:53:44 +0000

    (remove_idl_from_idl_list, remove_ide_from_idl): fixed the return value to be a true Boolean.

--- emu/slgdelay.c
+++ emu/slgdelay.c
@@ -493,7 +493,8 @@ static bool remove_ide_from_idl(IDE ide, IDLT *idl_tab_entry)
     if (idl == idlt_idl(*idl_tab_entry)) {
       idlt_idl(*idl_tab_entry) = ides_next_ide(idl);
     }
-    return (bool) (NULL == idlt_idl(*idl_tab_entry));
+    /* return (bool) idlt_idl(*idl_tab_entry); */
+    return (NULL != idlt_idl(*idl_tab_entry));
 }
 
 /*----------------------------------------------------------------------*/
@@ -517,7 +518,8 @@ static bool remove_idl_from_idl_list(IDLT idl_tab_entry, ASI *asi)
     if (idls == asi_idl_list(*asi)) {
       asi_idl_list(*asi) = idl_list_next(idls);
     }
-    return (bool) (NULL == asi_idl_list(*asi));
+    /* return (bool) asi_idl_list(*asi); */
+    return (NULL != asi_idl_list(*asi));
 }
 
 /*----------------------------------------------------------------------*/
