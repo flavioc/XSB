19d6130c02ea22f51ec936ec50e0ddf4f79e8657
cbaoqiu <cbaoqiu>
1998-12-09 03:14:37 +0000

    Changes to support variables in delay list, and changes of data structure of delay element/list.
    Specific changes in this file:
    
      Function build_delay_list() is rewritten to handle variables in
      delay list.

--- emu/residual.c
+++ emu/residual.c
@@ -1,5 +1,5 @@
 /* File:      residual.c
-** Author(s): Kostis Sagonas
+** Author(s): Kostis Sagonas, Baoqiu Cui
 ** Contact:   xsb-contact@cs.sunysb.edu
 ** 
 ** Copyright (C) The Research Foundation of SUNY, 1986, 1993-1998
@@ -56,102 +56,188 @@ static Cell cell_array[500];
 #define build_subgoal_args(SUBG)	\
 	load_solution_trie(arity, &cell_array[arity-1], subg_leaf_ptr(SUBG))
 
+
+CPtr *copy_of_var_addr;
+int copy_of_num_heap_term_vars;
+
 /*----------------------------------------------------------------------*/
 
-void build_delay_list(CPtr delay_list, IDEs idl)
-{
-    IDE  ide;
-    Psc  psc;
-    int  i, j, arity;
-    CPtr head, tail;
-    SGFrame subg;
-    NODEptr ans_subst;
+/*
+ * Function build_delay_list() is called by builtin #143 GET_DELAY_LISTS
+ * to construct on the heap the delay list pointed by `de'.  Since XSB
+ * 1.8.1, this function is changed to handle variables in delay list.
+ * Basically, to construct a delayed subgoal, we have to go through three 
+ * tries one by one: the call trie, answer trie, and delay trie of the
+ * delayed subgoal.  Delay trie contains the substitution factor of the
+ * answer for the delayed subgoal.
+ */
 
-#ifdef DEBUG_HEAP
+void build_delay_list(CPtr delay_list, DE de)
+{
+  Psc  psc;
+  int  i, j, arity;
+  CPtr head, tail;
+  SGFrame subg;
+  NODEptr ans_subst;
+  NODEptr subs_factp;		/* new vars */
+  CPtr *tmp_var_addr;
+  CPtr oldhreg = hreg;
+ 
+ 
+#ifdef HEAP_DEBUG
     fprintf(stderr, "delay_list starts at %p\n", hreg);
 #endif
     i = 0;
-    if (idl != NULL && !isnil(idl)) {
-      head = hreg+2;
+    if (de != NULL && !isnil(de)) {
+ 
       tail = hreg+1;
       bind_list(delay_list, hreg);
-      ide = ides_ide(idl);
-      subg = ide_subgoal(ide);
+      hreg = hreg + 3; 
+      build_delay_list(tail, de_next(de)); /* recursive call */
+      head = hreg;
+      subg = de_subgoal(de);
       psc = ti_psc_ptr(subg_tip_ptr(subg));
       arity = get_arity(psc);
-      if ((ans_subst = ide_ans_subst(ide)) == NULL) {
-	follow(hreg++) = makecs(head);
-	new_heap_functor(head, tnot_psc);
-	if (arity == 0) {
-	  bind_string(head, get_name(psc));
-	  hreg += 3;
-	} else {
-	  sreg = head+1;
-	  follow(head++) = makecs(sreg);
-	  hreg += arity+4; /* need arity(tnot)+2+arity(psc)+1 new cells */
-	  new_heap_functor(sreg, psc);
-	  for (j = 1; j <= arity; j++) {
-	    new_heap_free(sreg);
-	    cell_array[arity-j] = cell(sreg-1);
-	  }
-	  build_subgoal_args(subg);
-	}
-      } else {	/* positive delayed element */
-	if (arity == 0) {
-          new_heap_string(hreg, get_name(psc));
-	  hreg++;
+      if ((ans_subst = de_ans_subst(de)) == NULL) { /* Negative DE */
+        follow(oldhreg) = makecs(hreg);
+        new_heap_functor(head, tnot_psc);
+        if (arity == 0) {
+          bind_string(head, get_name(psc));
+          hreg += 3;
         } else {
-	  sreg = head;
-	  follow(hreg++) = makecs(head);
-	  hreg += arity+2;
-#ifdef DEBUG_HEAP
-	  fprintf(stderr, "Put a new functor %p at loc %p\n", psc, sreg);
+          sreg = head+1;
+          follow(head++) = makecs(sreg);
+          hreg += arity+4; /* need arity(tnot)+2+arity(psc)+1 new cells */
+          new_heap_functor(sreg, psc);
+          for (j = 1; j <= arity; j++) {
+            new_heap_free(sreg);
+            cell_array[arity-j] = cell(sreg-1);
+          }
+          build_subgoal_args(subg);
+        }
+      } else {					/* Positive DE */
+        if (arity == 0) {
+          new_heap_string(oldhreg, get_name(psc));
+        } else {
+	  /*
+	   * de_subs_fact(de) is the saved substitution factor for the
+	   * call of this delayed element.
+	   */
+          subs_factp = (NODEptr)de_subs_fact(de);
+          sreg = head;
+          follow(oldhreg) = makecs(head);
+          hreg += arity+1;
+          new_heap_functor(sreg, psc);
+          for (j = 1; j <= arity; j++) {
+            new_heap_free(sreg);
+            cell_array[arity-j] = cell(sreg-1);
+          }
+
+#ifdef DEBUG_DELAYVAR
+	  fprintf(stderr, ">>>> (before build_subgoal_args) num_heap_term_vars = %d\n", num_heap_term_vars);
 #endif
-	  new_heap_functor(sreg, psc);
-	  for (j = 1; j <= arity; j++) {
-	    new_heap_free(sreg);
-#ifdef DEBUG_HEAP
-	    fprintf(stderr, "Binding cell_array[%d] to %p\n",
-			     arity-j, (CPtr)cell(sreg-1));
-#endif
-	    cell_array[arity-j] = cell(sreg-1);
-	  }
+
+	  /*
+	   * Function build_subgoal_args() goes through the subgoal trie
+	   * and binds all the arguments of the subgoal skeleton, which
+	   * were built in the heap (like q(_,_)), to the actual call
+	   * arguments (like X and f(Y,g(Z)) in q(X, f(Y,g(Z)))).
+	   *
+	   * After build_subgoal_args, var_addr[] contains all the
+	   * variables in the subgoal call.
+	   */
 	  build_subgoal_args(subg);
-	  for (i = 0, j = num_heap_term_vars-1; j >= 0; j--) {
-	    cell_array[i++] = (Cell)var_addr[j];
-	  }
+
+#ifdef DEBUG_DELAYVAR
+	  fprintf(stderr, ">>>> (after build_subgoal_args) num_heap_term_vars = %d\n", num_heap_term_vars);
+#endif
+	  
+          for (i = 0, j = num_heap_term_vars-1; j >= 0; j--) {
+            cell_array[i++] = (Cell)var_addr[j];
+#ifdef DEBUG_DELAYVAR
+	    fprintf(stderr, ">>>> var_addr[%x] = %x\n", j, (int)var_addr[j]);
+#endif
+          }
+
+	  /*
+	   * Function load_solution_trie() goes through the answer trie
+	   * and binds the call substitution factor.  The substitution
+	   * factor of the answer is left in var_addr[].
+	   */
 	  load_solution_trie(i, &cell_array[i-1], ans_subst);
-	}
+
+#ifdef DEBUG_DELAYVAR
+	  fprintf(stderr, ">>>> (after load_solution_trie) num_heap_term_vars = %d\n", num_heap_term_vars);
+#endif
+
+	  for (i = 0, j = num_heap_term_vars-1; j >= 0; j--) {
+            cell_array[i++] = (Cell)var_addr[j];
+#ifdef DEBUG_DELAYVAR
+	    fprintf(stderr, ">>>> var_addr[%x] = %x\n", j, (int)var_addr[j]);
+#endif
+          }
+
+          tmp_var_addr = var_addr;
+
+	  /*
+	   * Restore var_addr[] to copy_of_var_addr[], which contains all
+	   * the variables in the _head_ predicate after get_returns is
+	   * called (see bineg.i).
+	   *
+	   * The content of copy_of_var_addr[] is used in
+	   * load_delay_trie(), and might be changed by each delay
+	   * element.  But during the whole process for one paticular
+	   * delay list, copy_of_var_addr[] will be shared by all the
+	   * delay elements.
+	   *
+	   * It is not necessary to restore copy_of_num_heap_term_vars
+	   * each time before load_delay_trie() is called for a delay
+	   * element.  Each variable in the trie can find its binding in
+	   * copy_of_var_addr[] without num_heap_term_vars.
+	   */
+	  var_addr = copy_of_var_addr; /* variables left in the head */
+          num_heap_term_vars = copy_of_num_heap_term_vars;
+
+
+#ifdef DEBUG_DELAYVAR
+	  fprintf(stderr, ">>>> NOW copy_of_num_heap_term_vars = %d\n",
+		  copy_of_num_heap_term_vars);
+          {
+            int i;
+            for(i = 0; i < num_heap_term_vars; i++)
+              fprintf(stderr, ">>>> var_addr[%d] = %x\n",i, (int)var_addr[i]);
+
+            fprintf(stderr, "Stored Subs Fact: <");
+            {
+              NODEptr x = subs_factp;
+	      if (x == NULL) fprintf(stderr, ">>>> subs_factp is NULL\n");
+              while(x != NULL){
+                print_trie_atom(Atom(x));
+                if(Sibl(x) != NULL) fprintf(stderr, "!");
+                x = Child(x);
+ 
+              }
+            }
+            fprintf(stderr, ">\n");
+          }
+	  fprintf(stderr, ">>>> num_heap_term_vars is %d before calling load_delay_trie\n", num_heap_term_vars);
+#endif /* DEBUG_DELAYVAR */
+	  
+          load_delay_trie(i, &cell_array[i-1],(NODEptr)de_subs_fact_leaf(de));
+
+#ifdef DEBUG_DELAYVAR
+	  fprintf(stderr, ">>>> num_heap_term_vars becomes %d\n",
+		  num_heap_term_vars);
+	  for (i = 0; i < num_heap_term_vars; i++)
+	    fprintf(stderr, ">>>> var_addr[%d] = %x\n",i, (int)var_addr[i]);
+#endif
+          var_addr = tmp_var_addr;
+        }
       }
-      build_delay_list(tail, ides_next_ide(idl));
+      hreg++;
     } else {
       bind_nil(delay_list);
     }
 }
-
-/*----------------------------------------------------------------------*/
-
-#ifdef DEBUG_RESIDUAL
-
-void fprint_delay_list(FILE *fp, IDEs idl)
-{
-    IDE ide;
-
-    fprintf(fp, "[");
-    while (idl != NULL) {
-      ide = ides_ide(idl);
-      if (ide_ans_subst(ide) == NULL) {
-	fprintf(fp, "tnot(");
-	print_subgoal(fp, ide_subgoal(ide));
-	fprintf(fp, ")");
-      } else {
-	print_subgoal(fp, ide_subgoal(ide));
-      }
-      if ((idl = ides_next_ide(idl)) != NULL) fprintf(fp, ", ");
-    }
-    fprintf(fp, "]");
-}
-
-#endif	/* DEBUG_RESIDUAL */
 
 /*---------------------- end of file residual.c ------------------------*/
