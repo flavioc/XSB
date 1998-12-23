e7fc57af90f1f85ff4098fb527d1a82a3bd346f3
kifer <kifer>
1998-12-23 05:24:18 +0000

    changed version number

--- build/version.sh
+++ build/version.sh
@@ -2,9 +2,9 @@
 
 xsb_major_version=1
 xsb_minor_version=9
-xsb_beta_version=6
-xsb_codename="Code Fixer"
+xsb_beta_version=7
+xsb_codename="Chatter"
 
 # Format: YYYY-MM-DD or YYYY.MM.DD or YYYY/MM/DD
-# With this, we should become Y2K compliant :-)
-xsb_release_date=1998-11-25
+# With this, XSB should become Y2K compliant :-)
+xsb_release_date=1998-12-23
