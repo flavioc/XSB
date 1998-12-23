f534ab408aaa765c4db8e3a8f24f83c4b0d35905
kifer <kifer>
1998-12-23 21:09:13 +0000

    improved detection of symlinks; commented out readlink() for NT

--- emu/self_orientation.c
+++ emu/self_orientation.c
@@ -137,6 +137,8 @@ char *xsb_executable_full_path(char *myname)
   static char myname_augmented[MAXPATHLEN];
 
   
+#ifndef WIN_NT
+  /* Unix */
   /* if we can read symlink, then it is a symlink */
   if ( (link_len = readlink(myname, myname_augmented, MAXPATHLEN)) > 0 ) {
     /* we can't assume that the value of the link is null-terminated */
@@ -144,8 +146,9 @@ char *xsb_executable_full_path(char *myname)
       *(myname_augmented+link_len+1) = '\0';
   } else
     strcpy(myname_augmented, myname);
-
-#ifdef WIN_NT
+#else
+  /* Windows doesn't seem to have readlink() */
+  strcpy(myname_augmented, myname);
   /* if executable doesn't end with .exe, then add it */
   if (strcmp(myname + strlen(myname) - 4, ".exe") != 0)
     sprintf(myname_augmented, "%s.exe", myname);
