fd78a23cf3194cecbd6365bceffa1d1ef4e6f162
kostis <kostis>
1999-02-10 18:07:10 +0000

    Useless macro deleted.

--- emu/tries.h
+++ emu/tries.h
@@ -25,8 +25,7 @@
 
 /* mark node as deleted */
 #define mark_leaf_node_del(X)  DelFlag(X) = 1
-#define del_ret_node(X)  DelFlag(aln_answer_ptr(X)) = 1
-#define is_del_ret_node(X)  DelFlag(aln_answer_ptr(X)) == 1 
+#define is_del_ret_node(X)     DelFlag(aln_answer_ptr(X)) == 1 
 
 #define is_not_deleted(X)      DelFlag(X) != 1
 
