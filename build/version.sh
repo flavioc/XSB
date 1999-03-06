ece54602b0d72c4805a777dc50332c13a80241fe
cbaoqiu <cbaoqiu>
1999-03-06 07:18:52 +0000

    Set version number (2.0) and name (Golfcart).

--- build/version.sh
+++ build/version.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-xsb_major_version=1
-xsb_minor_version=9
-xsb_beta_version=8
-xsb_codename="Chatter"
+xsb_major_version=2
+xsb_minor_version=0
+#xsb_beta_version=0
+xsb_codename="Golfcart"
 
 # Format: YYYY-MM-DD or YYYY.MM.DD or YYYY/MM/DD
 # With this, XSB should become Y2K compliant :-)
-xsb_release_date=1999-02-19
+xsb_release_date=1999-03-08
