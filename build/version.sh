7d9c8de692882e738ea52af5bbd0f2754b2153fc
kifer <kifer>
1999-04-01 05:01:03 +0000

    Made fmt_write_string be imported from file_io

--- build/version.sh
+++ build/version.sh
@@ -3,8 +3,8 @@
 xsb_major_version=2
 xsb_minor_version=0
 #xsb_beta_version=0
-xsb_codename="Golfcart"
+xsb_codename="Mouton Cadet"
 
 # Format: YYYY-MM-DD or YYYY.MM.DD or YYYY/MM/DD
 # With this, XSB should become Y2K compliant :-)
-xsb_release_date=1999-03-08
+xsb_release_date=1999-03-31
