937ecb2f1201a37f201b6ce82dc82ebf51b769b5
kifer <kifer>
1999-05-08 02:45:33 +0000

    test4

--- build/pkg_config.sh
+++ build/pkg_config.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
 
-# This runs XSB on some packages, so they will be compiled at the
-# installation time. Gets the full path name of the xsb executable as the
+# Run XSB on selected packages, so they wwould be compiled at the
+# installation time.
+# Gets the full path name of the XSB executable as the
 # first argument.
 
 xsb_executable=$1
+
 
 echo ""
 echo "Configuring XSB packages. Wait..."
