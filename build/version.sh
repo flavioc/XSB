f367c1e17b9b8d7260561148751a13923dc27db6
cbaoqiu <cbaoqiu>
1999-06-18 18:49:38 +0000

    *** empty log message ***

--- build/version.sh
+++ build/version.sh
@@ -3,8 +3,8 @@
 xsb_major_version=2
 xsb_minor_version=0
 #xsb_beta_version=0
-xsb_codename="Mouton Cadet"
+xsb_codename="Gouden Carolus"
 
 # Format: YYYY-MM-DD or YYYY.MM.DD or YYYY/MM/DD
 # With this, XSB should become Y2K compliant :-)
-xsb_release_date=1999-03-31
+xsb_release_date=1999-06-20
