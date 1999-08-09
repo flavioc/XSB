c445ff9a39a081295a45f74acab189f15b326afa
kifer <kifer>
1999-08-09 03:53:32 +0000

    fixed bug that was wiping out unix config.h

--- build/makexsb_wind.bat
+++ build/makexsb_wind.bat
@@ -1,5 +1,8 @@
-#  makexsb_wind.bat
+REM   makexsb_wind.bat
+REM   Script for compiling XSB under Windows using VC++
 
+del ..\emu\configs\config.h
+del ..\emu\debugs\debug.h
 copy ..\config\x86-pc-windows\MS_VC_Mfile.mak  ..\emu
 copy ..\config\x86-pc-windows\config.h  ..\emu\configs
 copy ..\config\x86-pc-windows\debug.h   ..\emu\debugs
