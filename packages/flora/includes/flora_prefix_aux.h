e99de953d839351a2609a9d709a1301a59bd3eb8
kifer <kifer>
1999-08-12 15:40:59 +0000

    Added a few more definitions, but this is probably not needed if we adopt the new prefix scheme

--- packages/flora/includes/flora_prefix_aux.h
+++ packages/flora/includes/flora_prefix_aux.h
@@ -19,10 +19,23 @@
 #define FLORA_WRITE_SHELL_PROMPT   '_$_$_flora_write_shell_prompt'
 
 #define FLORA_ASSERT	       	   '_$_$_flora_assert'
+#define FLORA_RETRACT	       	   '_$_$_flora_retract'
+#define FLORA_RETRACTALL       	   '_$_$_flora_retractall'
+#define FLORA_ERASE       	   '_$_$_flora_erase'
+
 #define FLORA_MIN    	     	   '_$_$_flora_min'
+#define FLORA_MAX    	     	   '_$_$_flora_max'
+#define FLORA_AVG    	     	   '_$_$_flora_avg'
+#define FLORA_SUM    	     	   '_$_$_flora_sum'
+#define FLORA_COLLECTSET     	   '_$_$_flora_collectset'
+#define FLORA_COLLECTBAG     	   '_$_$_flora_collectbag'
+
 #define FLORA_CHECK_LIBRARY 	   '_$_$_flora_check_library'
 
 #define FLORA_DEBUGGER_DATA_LOADED '_$_$_flora_debugger_data_loaded'
 #define FLORA_TRACE    	       	   '_$_$_flora_trace'
 #define FLORA_NOTRACE	     	   '_$_$_flora_notrace'
 #define FLORA_SWITCH	           '_$_$_flora_switch'
+
+#define FLORA_DEBUGGER_DATA_LOADED '_$_$_flora_debugger_data_loaded'.
+#define FLORA_DEBUG_GET_CONTEXT	   '_$_$__flora_debug_get_context'.
