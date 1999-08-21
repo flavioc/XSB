873cb72c82f05ac9cc1df7b643733e6732f42c1a
kifer <kifer>
1999-08-21 02:55:24 +0000

    changed version/date number

--- build/version.sh
+++ build/version.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
 xsb_major_version=2
-xsb_minor_version=0
+xsb_minor_version=01
 #xsb_beta_version=0
 xsb_codename="Gouden Carolus"
 
 # Format: YYYY-MM-DD or YYYY.MM.DD or YYYY/MM/DD
 # With this, XSB should become Y2K compliant :-)
-xsb_release_date=1999-06-27
+xsb_release_date=1999-08-20
