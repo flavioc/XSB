526d0c61e616222c09b5a81f44dd890a92c6695e
kifer <kifer>
1999-10-05 04:03:07 +0000

    Added the initial libwww stuff (buggy)

---
+++ packages/libwww/cc/xsb_libwww.c
@@ -0,0 +1,283 @@
+#include "WWWLib.h"
+#include "WWWHTTP.h"
+#include "WWWInit.h"
+
+#include "cinterf.h"
+#include "basictypes.h"
+
+#include "xsb_libwww_util.c"
+
+HTRequest * request = NULL;
+HTResponse * response = NULL;
+HTChunk * chunk = NULL;
+HTParentAnchor * anchor = NULL;
+
+HTAssocList * headers = NULL;
+HTAssocList *cur = NULL;
+HTAssoc * pres;
+
+prolog_term Request_term, Response_term;
+prolog_term Form_list, element_list, head, tail;
+
+char *url, *cwd, *absolute_url, *string;
+char * response_array[MAX];
+prolog_term * modified_time;
+char * last_modified;
+int timeout_integer;
+int load;
+load = FALSE;
+
+void *context;
+context =(HTBasic *) malloc(sizeof(HTBasic));
+
+/* after filter for all retcodes other than HT_NO_ACCESS and HT_NO_PROXY_ACCESS */
+int HTInfoFilter (HTRequest * request, HTResponse * response, void * param,
+				int status) 
+{ 	
+	int i;
+	for (i=0; i<48; i++) {
+		if (status_msg[i].code == status) {
+			response_array[0] = "status";
+			response_array[1] = status_msg[i].message;
+			response_array[2] = status_msg[i].type;
+			response_array[3] = NULL;
+			add_response(&Response_term, response_array);
+		}
+	}
+}
+
+/* after filter for HT_NO_ACCESS or HT_NO_PROXY_ACCESS retcodes */
+int HTAuthFilter (HTRequest * request, HTResponse * response, void * param,
+				int status) {	
+	int i;
+	for (i=0; i<48; i++) {
+		if (status_msg[i].code == status) {
+			response_array[0] = "status";
+			response_array[1] = status_msg[i].message;
+			response_array[2] = status_msg[i].type;
+			response_array[3] = NULL;
+			add_response(&Response_term, response_array);
+		}
+	}
+
+	if (HTResponse_challenge(response)) {
+		Basic_generate(request, context, status);
+		HTLoad(request, NO);
+		return HT_ERROR;
+	}
+	return HT_OK;
+}
+
+prolog_term get_request_member(prolog_term V)
+{ 
+	char * str;
+	str = p2c_functor(V);
+	if (str == "timeout") 
+		timeout_integer = ptoc_int(p2p_arg(V, 1));
+	if (str == "if_modified_since")
+		modified_time = p2p_arg(V, 1);
+	if (str == "authorization") 
+	{ 
+		context->uid = ptoc_string(p2p_arg(V, 1));
+		context->pw = ptoc_string(p2p_arg(V, 2));
+	}
+	if (str == "head") Load = FALSE;
+	if (str == "form_list")	Form_list = p2p_arg(V, 1);		
+	if (str == "form_method") 
+		method = ptoc_string(p2p_arg(V, 1));
+	return true;
+}
+
+add_response(prolog_term listTail, char * array)
+{
+	int i, j;
+	prolog_term listHead, listTail;
+	i=0;
+	j=0;
+	listHead = p2p_car(listTail);
+	while (array[i++] != NULL); 
+	c2p_functor(array[0], i-2, listHead);
+	while (array[++j] != NULL) 
+		c2p_string(array[j], p2p_arg(listHead, j));
+	listTail = p2p_cdr(listTail);
+}
+
+load_header(HTRequest * request)
+{
+	HTHeadAbsolute(url, request); 
+	anchor = HTRequest_anchor(request);
+	headers = HTAnchor_header(anchor);
+
+	if (headers) {
+		cur = headers;
+		while ((pres = (HTAssoc *) HTAssocList_nextObject(cur))) {
+			char * name = HTAssoc_name(pres);
+			char * value = HTAssoc_value(pres);
+			response_array[0] = name;
+			response_array[1] = value;
+			response_array[2] = NULL;
+			add_response(&Response_term, response_array);
+				
+			if (HTStrCaseMatch("Last-Modified", name)) 
+			last_modified = value;
+		}
+	}
+}
+
+bool do_libwww_fetch_url___(void) 
+{	/* char *url, prolog_term Request, Prolog_term Response */ 
+	url = ptoc_string(1);
+	Request_term = reg_term(2);
+	Response_term = reg_term(3);
+
+	if (!is_list(Request_term)) xsb_abort("Request_term not a list!");
+	  
+	/* get the request members from the Request_term list */
+	while (!is_nil(Request_term)) 
+	{		
+		head = p2p_car(Request_term);
+		get_request_member(head);
+		Request_term = p2p_cdr(Request_term);
+	}
+
+	HTProfile_newPreemptiveClient("fetch_url", "1.0");
+ 
+	HTNet_addAfter(HTInfoFilter, NULL, NULL, HT_ALL, HT_FILTER_LAST);
+ 	HTNet_addAfter(HTAuthFilter, NULL, NULL, HT_NO_ACCESS, HT_FILTER_LAST);
+ 	HTNet_addAfter(HTAuthFilter, NULL, NULL, HT_NO_PROXY_ACCESS, HT_FILTER_LAST);
+
+	if (timeout_integer) HTHost_setEventTimeout(timeout_integer);
+
+	/* make Response_term a list */
+	c2p_list(Response_term);
+
+	if (url) {
+		request = HTRequest_new();
+		cwd = HTGetCurrentDirectoryURL();
+		HTRequest_setOutputFormat(request, WWW_SOURCE);             
+		HTRequest_addConnection(request, "close", "");
+
+		absolute_url = HTParse(url, cwd, PARSE_ALL);
+		chunk =	HTLoadToChunk(absolute_url, request);
+ 		HT_FREE(absolute_url);
+		HT_FREE(cwd);
+
+	} else 
+		xsb_abort("Bad parameters - please try again\n");
+		
+	/* check for the newness of last_modified time */ 
+	if (modified_time != NULL) 
+		time_comparison(last_modified, modified_time);
+	else 
+		load = TRUE;
+
+	if (is_nil((Request_term) || (load == TRUE)) {
+		if (chunk) {
+			string = HTChunk_toCString(chunk);
+			response_array[0] = "content"; 
+			response_array[1] = string;
+			response_array[2] = NULL;
+			add_response(&Response_term, response_array);
+			HT_FREE(string);
+		} 
+	}	
+	
+	/* get the header information and add to the response_term list */
+	load_header(request);
+
+	HTRequest_delete(request);
+	HTProfile_delete();
+}
+
+
+
+bool do_libwww_form_request___(void) 
+{	/* form_request(+Url, +Request, -Response) */
+	HTAssocList * Form_fields = NULL;
+	HTAnchor * anchor = NULL;
+	char *string, *value;
+	url = ptoc_string(1);
+	Request_term = reg_term(2);
+	Response_term = reg_term(3);
+			
+	HTProfile_newNoCacheClient("form-request", "1.0");	
+	HTNet_addAfter(HTInfoFilter, NULL, NULL, HT_ALL, HT_FILTER_LAST);
+ 	HTNet_addAfter(HTAuthFilter, NULL, NULL, HT_NO_ACCESS, HT_FILTER_LAST);
+ 	HTNet_addAfter(HTAuthFilter, NULL, NULL, HT_NO_PROXY_ACCESS, HT_FILTER_LAST);
+
+	if (!is_list(Request_term)) xsb_abort("Request_term not a list!");
+
+	/* get the request members from the Request_term list */
+	while (!is_nil(Request_term)) 
+	{		
+		head = p2p_car(Request_term);
+		get_request_member(head);
+		Request_term = p2p_cdr(Request_term);
+	}
+	c2p_nil(Request_term);
+	
+	if (!is_list(Form_list)) xsb_abort("form_list must be a list!");
+
+	while (!is_nil(Form_list)) {
+		element_list = p2p_car(Form_list);
+		if (!is_list(element_list)) xsb_abort("wrong form list pattern!");
+		head = p2p_car(element_list);
+		tail = P2p_cdr(element_list);
+		c2p_string(name, head);
+		c2p_string(value, tail);
+		c2p_nil(element_list);
+		Form_list = p2p_cdr(Form_list);
+		strcat(name, value);
+		
+		/* create a list to hold the form arguments */
+		 if (!Form_fields) Form_fields = HTAssocList_new();
+		
+		/* parse the content and add it to the association list */
+		HTParseFormInput(Form_fields, name);
+	}
+
+	if (timeout_integer) HTHost_setEventTimeout(timeout_integer);
+
+	if (url && Form_fields) {
+		request = HTRequest_new(); 
+		HTRequest_setOutputFormat(request, WWW_SOURCE);             
+		/* get an anchor object for the url */
+		anchor = HTAnchor_findAddress(url);
+		if (method == "POST") {
+			/* post the data and get the result in a chunk */
+			result = HTPostFormAnchorToChunk(Form_fields, anchor, request);
+
+			load_header(request);
+	
+			/* If result != NULL then we have the data */
+			if (result) {
+				/* convert the chunk to C string */
+				string = HTChunk_toCString(result);
+				response_array[0] = "content"; 
+				response_array[1] = string;
+				response_array[2] = NULL;
+				add_response(&Response_term, response_array);
+				HT_FREE(string);
+			} 
+		} else if (method == "GET") {
+				/* get the data and get the result in a chunk */
+				result = HTGetFormAnchorToChunk(Form_fields, anchor, request);
+			
+				load_header(request);
+
+				/* If result != NULL then we have the data */
+				if (result) {
+					/* convert the chunk to C string */
+					string = HTChunk_toCString(result);
+					response_array[0] = "content"; 
+					response_array[1] = string;
+					response_array[2] = NULL;
+					add_response(&Response_term, response_array);
+					HT_FREE(string);
+				}
+	} else
+		xsb_abort("Bad parameters - please try again\n");
+	
+	HTRequest_delete(request);
+	HTProfile_delete();
+}
---
+++ packages/libwww/cc/xsb_libwww_util.c
@@ -0,0 +1,165 @@
+#include "xsb_libwww_util.h"
+#include <time.h>
+
+typedef time_t int;
+
+HTBasic *HTBasic_new()
+{
+	HTBasic * me = NULL;
+	if ((me= (HTBasic *) HT_CALLOC(1, sizeof(HTBasic))) == NULL)
+		HT_OUTOFMEM("HTBasic_new");
+	return me;
+}
+
+/*
+**      Make basic authentication scheme credentials and register this
+**      information in the request object as credentials. They will then
+**      be included in the request header. An example is 
+**
+**              "Basic AkRDIhEF8sdEgs72F73bfaS=="
+**
+**      The function can both create normal and proxy credentials
+**      Returns HT_OK or HT_ERROR
+*/
+
+BOOL Basic_credentials (HTRequest * request, HTBasic * basic)
+{
+    if (request && basic) {
+        char * cleartext = NULL;
+        char * cipher = NULL;
+        int cl_len = strlen(basic->uid ? basic->uid : "") +
+            strlen(basic->pw ? basic->pw : "") + 5;
+        int ci_len = 4 * (((cl_len+2)/3) + 1);
+        if ((cleartext = (char *) HT_CALLOC(1, cl_len)) == NULL)
+            HT_OUTOFMEM("basic_credentials");
+        *cleartext = '\0';
+        if (basic->uid) strcpy(cleartext, basic->uid);
+        strcat(cleartext, ":");
+        if (basic->pw) strcat(cleartext, basic->pw);
+        if ((cipher = (char *) HT_CALLOC(1, ci_len + 3)) == NULL)
+            HT_OUTOFMEM("basic_credentials");
+        HTUU_encode((unsigned char *) cleartext, strlen(cleartext), cipher);
+
+        /* Create the credentials and assign them to the request object */
+        {
+            int cr_len = strlen("basic") + ci_len + 3;
+            char * cookie = (char *) HT_MALLOC(cr_len+1);
+            if (!cookie) HT_OUTOFMEM("basic_credentials");
+            strcpy(cookie, "Basic ");
+            strcat(cookie, cipher);
+            HTTRACE(AUTH_TRACE, "Basic Cookie `%s\'\n" _ cookie);
+
+            /* Check whether it is proxy or normal credentials */
+            if (basic->proxy)
+                HTRequest_addCredentials(request, "Proxy-Authorization", cookie);
+            else
+                HTRequest_addCredentials(request, "Authorization", cookie);
+
+            HT_FREE(cookie);
+        }
+        HT_FREE(cleartext);
+        HT_FREE(cipher);
+        return HT_OK;
+    }
+    return HT_ERROR;
+}
+
+/*      HTBasic_generate
+**      ----------------
+**      This function generates "basic" credentials for the challenge found in
+**      the authentication information base for this request. The result is
+**      stored as an association list in the request object.
+**      This is a callback function for the AA handler.
+*/
+int Basic_generate (HTRequest * request, void * context, int mode)
+{ 
+    HTBasic * basic = (HTBasic *) context;
+    BOOL proxy = (mode==HT_NO_PROXY_ACCESS) ? YES : NO;
+    if (request) {
+        const char * realm = HTRequest_realm(request);
+
+        if (!basic) {
+                basic = HTBasic_new();
+            if (proxy) {
+                char * url = HTRequest_proxy(request);
+                basic->proxy = YES;
+                HTAA_updateNode(proxy, BASIC_AUTH, realm, url, basic);
+            } else {
+                char * url = HTAnchor_address((HTAnchor*)HTRequest_anchor(request));
+                HTAA_updateNode(proxy, BASIC_AUTH, realm, url, basic);
+                HT_FREE(url);
+            }
+        }
+
+		Basic_credentials(request, basic);
+        char * url = HTAnchor_address((HTAnchor*)HTRequest_anchor(request));
+        HTAA_deleteNode(proxy, BASIC_AUTH, realm, url);
+        HT_FREE(url);
+        return HT_ERROR;
+        
+    }
+    return HT_OK;
+}
+
+HTChunk * HTGetFormAnchorToChunk (HTAssocList * formdata,
+                                  HTAnchor *    anchor,
+                                  HTRequest *   request)
+{
+    if (formdata && anchor && request) {
+        HTChunk * chunk = NULL;
+        HTStream * target = HTStreamToChunk(request, &chunk, 0);
+        HTRequest_setOutputStream(request, target);
+        if (HTGetFormAnchor(formdata, anchor, request) != NULL)
+            return chunk;
+        else {
+            HTChunk_delete(chunk);
+            return NULL;
+        }
+    }
+    return NULL;
+}
+
+void time_comparison(lm_time, time_user) 
+{
+/*time1 is ptr to the last-modified time, time2 is ptr to the user input time*/
+		struct tm *time1, *time2;
+		char seps[] = ", :";
+
+		/* get the tm struct element from the time1 */
+		strtok(time1, seps);
+		time1->tm_mday = atoi(strtok(lm_time, seps)));
+		time1->tm_mon = atoi(strtok(lm_time, seps)));
+		time1->tm_year = atoi(strtok(lm_time, seps)));
+		time1->tm_hour = atoi(strtok(lm_time, seps)));
+		time1->tm_min = atoi(strtok(lm_time, seps)));
+		time1->tm_sec = atoi(strtok(lm_time, seps)));
+
+		/* extract the if_modified_since infomation into struct tm */ 
+		c2p_int(time2->tm_mday, p2p_car(time2));
+		time2 = p2p_cdr(time_user);
+		c2p_int(time2->tm_mon, p2p_car(time2));
+		time2 = p2p_cdr(time_user);
+		c2p_int(time2->tm_year, p2p_car(time2));
+		time2 = p2p_cdr(time_user);
+		c2p_int(time2->tm_hour, p2p_car(time2));
+		time2 = p2p_cdr(time_user);
+		c2p_int(time2->tm_min, p2p_car(time2));
+		time2 = p2p_cdr(time_user);
+		c2p_int(time2->tm_sec, p2p_car(time2));
+		c2p_nil(time_user);
+		
+		time_t lm_sec, user_sec; 
+	
+		/* mktime and compare the time1 time with the
+		if_modified_since time */
+		
+		lm_sec = mktime(time1);
+		user_sec = mktime(time2);
+
+		if (lm_sec != -1 || user_sec != -1) {
+			if (lm_sec > user_sec) LOAD = TRUE;
+			else LOAD = FALSE;
+		}
+		else 
+			xsb_abort("the user-specified time or the last-modified time 			is not correct\n");
+}
