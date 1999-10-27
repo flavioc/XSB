7a175c4236a6041ab7b4303788ca1c82bfb03f9f
kifer <kifer>
1999-10-27 06:03:08 +0000

    LD_LIBRARY_PATH is now automatically set from the header of the .O file. This means that any -L option specified in ld_options is automatically taken care of, and the user doesn't need to set LD_LIBRARY_PATH before letting XSB load a foreign module.

--- emu/dynelf_xsb_i.h
+++ emu/dynelf_xsb_i.h
@@ -64,10 +64,11 @@ static byte *load_obj_dyn(char *pofilename, Psc cur_mod, char *ld_option)
   void	*handle;
   void	*funcep;
   bool	dummy();
-/*   char  *ldp1,*ldp2; */
-/*   char  *ldstring,*libpath; */
-/*   char  ldtemp; */
-/*   int   slibpath; */
+  char  *ldp1,*ldp2;
+  static char  *ldstring_oldenv, *ldstring_newenv;
+  char  *libpath;
+  char  ldtemp; 
+  int   slibpath;
   
   /* (1) create filename.so */
   
@@ -78,54 +79,69 @@ static byte *load_obj_dyn(char *pofilename, Psc cur_mod, char *ld_option)
   
   /* (1.5) include necessary paths into LD_LIBRARY_PATH */
   
-/*   libpath = getenv("LD_LIBRARY_PATH"); */
-/*   slibpath = strlen(libpath); */
-/*   ldstring = (char *) malloc(sizeof(char)*(strlen(ld_option)+slibpath+17)); */
+  libpath = getenv("LD_LIBRARY_PATH");
+  if (libpath == NULL)
+    libpath = "";
+  slibpath = strlen(libpath);
 
-/*   if (ldstring == NULL) */
-/*     xsb_abort("Could not allocate memory for ld_options manipulation"); */
-
-/*   *ldstring = '\0'; */
-/*   ldstring = strcpy(ldstring,"LD_LIBRARY_PATH="); */
-/*   ldstring = strcat(ldstring,libpath); */
+  ldstring_newenv = (char *) malloc(sizeof(char)*(strlen(ld_option)
+						  +slibpath
+						  +sizeof("LD_LIBRARY_PATH=")
+						  +1));
+  ldstring_oldenv = (char *) malloc(sizeof(char)*(strlen(ld_option)
+						  +slibpath
+						  +sizeof("LD_LIBRARY_PATH=")
+						  +1));
   
-/*   for (ldp1=ld_option; (*ldp1); ldp1++) { */
-/*     if (*ldp1 == '-' && *(ldp1+1) == 'L') { */
-/*       ldp1 += 2; */
-/*       ldp2 = ldp1; */
-/*       while (*ldp1 != ' ' && *ldp1 != '\0') */
-/* 	ldp1++; */
-/*       *ldp1 = '\0'; */
-/*       ldtemp = *(ldp2-1); */
-/*       *(ldp2-1) = ':'; */
-/*       ldstring = strcat(ldstring,ldp2-1); */
-/*       *ldp1 = ' '; */
-/*       *(ldp2-1) = ldtemp; */
-/*     } else if (*ldp1 == '\'') { */
-/*       ldp1++; */
-/*       while (*ldp1 != '\'') */
-/* 	ldp1++; */
-/*     } else if (*ldp1 == '\"') { */
-/*       ldp1++; */
-/*       while (*ldp1 != '\"') */
-/* 	ldp1++; */
-/*     } */
-/*   } */
-
-/*   if (putenv(ldstring) != 0) */
-/*     xsb_warn("+++Error changing environment variable LD_LIBRARY_PATH"); */
-
-  /*   free(ldstring); */
-  /* the call above segfaults on linux when calling getenv afterwards */
-  /* luis, 10.21.99 */
-
+  if (ldstring_newenv == NULL)
+    xsb_abort("Could not allocate memory for ld_options manipulation");
+  
+  *ldstring_oldenv = '\0';
+  ldstring_oldenv = strcpy(ldstring_oldenv,"LD_LIBRARY_PATH=");
+  ldstring_oldenv = strcat(ldstring_oldenv,libpath);
+  *ldstring_newenv = '\0';
+  ldstring_newenv = strcpy(ldstring_newenv,"LD_LIBRARY_PATH=");
+  ldstring_newenv = strcat(ldstring_newenv,libpath);
+  
+  /* search for -Lpath, -L"paths" or -L'paths' */
+  for (ldp1=ld_option; (*ldp1); ldp1++) {
+    if (*ldp1 == '-' && *(ldp1+1) == 'L') {
+      ldp1 += 2;
+      ldp2 = ldp1;
+      while (*ldp1 != ' ' && *ldp1 != '\0')
+ 	ldp1++;
+      *ldp1 = '\0';
+      ldtemp = *(ldp2-1);
+      *(ldp2-1) = ':';
+      ldstring_newenv = strcat(ldstring_newenv,ldp2-1);
+      *ldp1 = ' ';
+      *(ldp2-1) = ldtemp;
+    } else if (*ldp1 == '\'') {
+      ldp1++;
+      while (*ldp1 != '\'')
+	ldp1++;
+    } else if (*ldp1 == '\"') {
+      ldp1++;
+      while (*ldp1 != '\"')
+ 	ldp1++;
+    }
+  }
+  
+  if (putenv(ldstring_newenv) != 0)
+    xsb_abort("LOAD_OBJ_DYN: couldn't change the environment variable LD_LIBRARY_PATH");
   
   /* (2) open the needed object */
   
   if (( handle = dlopen(sofilename, RTLD_LAZY)) == 0 ) {
     xsb_mesg("%s", dlerror());
-    return 0;
+    return FALSE;
   }
+
+  if (putenv(ldstring_oldenv) != 0)
+    xsb_abort("LOAD_OBJ_DYN: couldn't change the environment variable LD_LIBRARY_PATH");
+  
+  free(ldstring_oldenv);
+  free(ldstring_newenv);
   
   /* (3) find address of function and data objects
   **
