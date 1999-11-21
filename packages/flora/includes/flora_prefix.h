77faf5d2d28a1456a990db30739cc9d563313950
kifer <kifer>
1999-11-21 05:52:26 +0000

    Streamlined flora's preprocessor macros. Took them out of the files into a separate flora_prefix.h Replaced non-ground tnot's with safe tnots.

--- packages/flora/includes/flora_prefix.h
+++ packages/flora/includes/flora_prefix.h
@@ -1,21 +1,31 @@
 
-/* Variable Flora prefix: this prefix changes dynamically 
-    #define VAR_FPREFIX(X)    '_$_$_flora_' ## X
-    // Variable Flora prefix for dynamically loaded files: changes dynamically
-    #define DYN_FPREFIX(X)    '_$_$_flora_dyn_' ## X
-    // Static Flora prefix, for standard or kitchen-sink predicates
-    #define STD_FPREFIX(X)    '_$_$_flora_' ## X
-    // Use:
-   VAR_FPREFIX('isa'(X,Y)).
-   DYN_FPREFIX('fd'(O,M,X)).
-   SYN_FPREFIX('isa'/2).
+/* Variable Flora prefix changes dynamically 
+   Use:
+       #define VAR_FPREFIX_BASE ...
+   
+   Examples:
+       VAR_FPREFIX(isa)(X,Y)
+       STD_FPREFIX(isa)/2.
 */
 
-#define VAR_FPREFIX(X)    '_$_$_flora_''X'
-#define DYN_FPREFIX(X)    '_$_$_flora_dyn_''X'
-#define STD_FPREFIX(X)    '_$_$_flora_''X'
+#mode standard Prolog
 
-/* Use:
-   VAR_FPREFIX(isa)(X,Y).
-*/
+#define STD_FPREFIX_BASE  _$_$_flora_
+
+#if !defined(VAR_FPREFIX_BASE)
+#define VAR_FPREFIX_BASE  STD_FPREFIX_BASE
+#endif
+
+/* Tell GPP to stop interpreting ' as a string while evaluating these macros
+   The result is that these macros will be substituting inside the '...' */
+#mode save
+#mode nostring "\!#'"
+#define VAR_FPREFIX(X)    'VAR_FPREFIX_BASE''X'
+#define STD_FPREFIX(X)    'STD_FPREFIX_BASE''X'
+#mode restore
+
+
+:- op(1050, xfy, STD_FPREFIX(then)).
+:- op(1100, xfy, STD_FPREFIX(else)).
+
 
