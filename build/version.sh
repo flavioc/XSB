7f17e97a8f2b4bea66be90f9622472ab26890669
luis <luis>
1999-11-23 23:47:12 +0000

    - changed codename & revision for new release

--- build/version.sh
+++ build/version.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
 xsb_major_version=2
-xsb_minor_version=01
+xsb_minor_version=02
 #xsb_beta_version=0
-xsb_codename="Gouden Carolus"
+xsb_codename="Skol"
 
 # Format: YYYY-MM-DD or YYYY.MM.DD or YYYY/MM/DD
 # With this, XSB should become Y2K compliant :-)
-xsb_release_date=1999-08-20
+xsb_release_date=1999-11-25
