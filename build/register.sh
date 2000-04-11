1dc523c275d0a0fb8c570521f1186d35f4014d50
kifer <kifer>
2000-04-11 22:30:27 +0000

    updated bug reporting info

--- build/register.sh
+++ build/register.sh
@@ -6,11 +6,11 @@ cat <<EOF
 
 The installation process is now complete. The log is in: Installation_summary
 
-We shall greatly appreciate, if you would agree to send us this log.
+We would like to ask you to email this log to us.
 Installation logs help the XSB group to keep track of the usage of the
 system on different architectures and to isolate problems more easily.
 
-The log will be sent automatically to  \`xsb-installation@lists.sourceforge.net'
+The log will be sent automatically to  xsb-installation@lists.sourceforge.net
 Would you like to send us the installation log? (y/n): y
 EOF
 
@@ -21,4 +21,13 @@ if test "$sendlog" != "n" -a "$sendlog" != "no" -a "$sendlog" != "N" ; then
 	| mail xsb-installation@lists.sourceforge.net) \
     && echo "" ; echo "Thank you!"; echo ""
 fi
+
+cat <<EOF
+Should you find a bug in XSB, please report it using our bug tracking system at
+
+	http://sourceforge.net/bugs/?group_id=1176 
+
+and also to  xsb-development@lists.sourceforge.net
+
+EOF
 
