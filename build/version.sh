930bf66690116aa8fdecf9152d2a0be7d5fa55b1
cbaoqiu <cbaoqiu>
2000-04-20 21:07:20 +0000

    Changed version number from 2.1 to 2.2, and code name from Skol to Tsingtao.

--- build/version.sh
+++ build/version.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 
 xsb_major_version=2
-xsb_minor_version=1
+xsb_minor_version=2
 #xsb_beta_version=0
-xsb_codename="Skol"
+#xsb_codename="Skol"		# for Version 2.1
+xsb_codename="Tsingtao"		# for Version 2.2
 
 # Format: YYYY-MM-DD or YYYY.MM.DD or YYYY/MM/DD
 # With this, XSB should become Y2K compliant :-)
-xsb_release_date=1999-11-25
+xsb_release_date=2000-04-20
