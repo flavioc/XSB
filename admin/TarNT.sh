2ace44e3b827326a44f74499eb3bc8210fb0498e
kifer <kifer>
2000-06-07 03:55:47 +0000

    added files to tar

--- admin/TarNT.sh
+++ admin/TarNT.sh
@@ -9,7 +9,7 @@ files="./XSB/LICENSE ./XSB/INSTALL ./XSB/INSTALL_PROBLEMS \
 	./XSB/admin \
         ./XSB/build/ac* ./XSB/build/*.in ./XSB/build/config.guess \
      	./XSB/build/config.sub ./XSB/build/*sh ./XSB/build/*.msg \
-    	./XSB/build/*.bat \
+    	./XSB/build/*.bat ./XSB/build/*.sed \
     	./XSB/build/configure ./XSB/build/README \
         ./XSB/emu ./XSB/syslib ./XSB/cmplib ./XSB/lib \
 	./XSB/gpp \
