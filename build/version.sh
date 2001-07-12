a9faceea46f31ff04e55d07e51b941bc96172ea5
lfcastro <lfcastro>
2001-07-12 14:55:05 +0000

    * new release!

--- build/version.sh
+++ build/version.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
 xsb_major_version=2
-xsb_minor_version=3
+xsb_minor_version=4
 #xsb_beta_version=0
 #xsb_codename="Skol"		# for Version 2.1
 #xsb_codename="Tsingtao"		# for Version 2.2
-xsb_codename="Zombie"          # Version 2.3
+#xsb_codename="Zombie"          # Version 2.3
+xsb_codename="Bavaria"          # the beer from Holland
 
 # Format: YYYY-MM-DD or YYYY.MM.DD or YYYY/MM/DD
 # With this, XSB should become Y2K compliant :-)
-xsb_release_date=2001-02-16
+xsb_release_date=2001-07-13
