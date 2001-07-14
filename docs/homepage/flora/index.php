c82c5036d1f5e52a96a64463da323d8d2f05c481
kifer <kifer>
2001-07-14 04:18:16 +0000

    added pdf, updated flora web page

--- docs/homepage/flora/index.php
+++ docs/homepage/flora/index.php
@@ -16,20 +16,32 @@
 This page is in alpha state. Please excuse our appearance.
 
 <p>
-FLORA is a sophisticated F-logic to <a
-href="http://www.cs.sunysb.edu/~sbprolog/xsb-page.html">XSB</a>
-compiler. It supports all features of F-logic, except type checking, and
-provides many extensions that make it a powerful programming platform. In
-particular, it supports HiLog and Transaction Logic. In addition, FLORA is
-seamlessly integrated with the XSB programming environment, which enables
-the development of large systems with components implemented in FLORA,
-Prolog, C, etc.
+FLORA-2 is a sophisticated deductive object-oriented database system and
+application development platform. It is implemented as a compiler that
+translates a unified language of F-logic, HiLog, and Transaction Logic into
+<a href="http://www.cs.sunysb.edu/~sbprolog/xsb-page.html">
+the XSB deductive engine</a>.
+
+The programming language supported by FLORA-2 is a dialect of F-logic with
+numerous extensions. Some extensions, such as path expressions, are
+borrowed from <a
+href="http://www.informatik.uni-freiburg.de/~dbis/">FLORID</a>.  However,
+FLORA-2 has unique and powerful features not found in other deductive
+object-oriented database systems.  First, as already mentioned, in addition
+to F-logic, it supports HiLog and logical updates, as in Transaction Logic.
+Second, FLORA-2 was designed with extensibility and flexibility in mind.
+The operator grammar is carefully crafted into the language, which allows the
+user to extend the syntax with her own idioms.  Third, FLORA-2 provides an
+original dynamic module system, which supports modular software design and
+turns it into a flexible application development platform.
+
+Applications of FLORA-2 include Intelligent Agents, Semantic Web,
+and Ontology management.
 
 <p>
-We are happy to announce the release of an alpha version of FLORA-2, a
-completely overhauled re-implementation of FLORA, which now supports HiLog
-and Transaction Logic, a flexible module system, and much more. FLORA-2 is
-<b>not</b> syntactically compatible with the earlier version of FLORA.
+We are happy to announce the release of an alpha version of FLORA-2.
+Note: FLORA-2 is <b>not</b> syntactically compatible with the earlier
+prototype, called FLORA. 
 <p>
 Here is the manual:
 	<ul>
@@ -38,12 +50,13 @@ Here is the manual:
 	</ul>
 
 <p>
-Other useful links:
+Related links:
 <ul>
 <li> <a href="http://www.informatik.uni-freiburg.de/~dbis/florid/">
     FLORID</a>: A C++-based implementation of F-logic from Freiburg University.
 <li> <A href="http://www.dsic.upv.es/~pcarsi/tfl/">Transaction F-logic
   prototype</a> from the University of Valencia, Spain.
+<li> <a href="http://www.cs.toronto.edu/~bonner/transaction-logic.html">The Transaction Logic Page</a>
 <li> <A href="http://www.cs.sunysb.edu/~kifer/dood/">The DOOD page</a>:
     additional links to information on Deductive and Object-Oriented
     Databases.
