0abaf41b45c399386940b49a4a2e76d0ce427fdb
kifer <kifer>
2001-10-09 05:48:20 +0000

    fixed the time.h problem

--- emu/xsb_time.h
+++ emu/xsb_time.h
@@ -29,10 +29,15 @@
 
 #else /* Unix */
 
+#if TIME_WITH_SYS_TIME
+#include <sys/time.h>
+#include <time.h>
+#else
 #ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
 #else
 #include <time.h>
+#endif
 #endif
 
 #endif
