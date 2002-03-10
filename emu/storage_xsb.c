5497a897ee46d79d9f71d76ab8cc4c61949f622c
kifer <kifer>
2002-03-10 05:06:15 +0000

    efficiency improvements in storage_* primitives.

--- emu/storage_xsb.c
+++ emu/storage_xsb.c
@@ -42,12 +42,52 @@
 #include "storage_xsb.h"
 #include "tr_utils.h"
 
-#include "conget_xsb_i.h"
 
-STORAGE_HANDLE *get_storage_handle(Cell name);
-STORAGE_HANDLE *increment_storage_snapshot(Cell name);
-STORAGE_HANDLE *mark_storage_changed(Cell name);
-STORAGE_HANDLE *destroy_storage_handle(Cell name);
+static inline STORAGE_HANDLE *get_storage_handle(Cell name);
+static STORAGE_HANDLE        *increment_storage_snapshot(Cell name);
+static STORAGE_HANDLE        *mark_storage_changed(Cell name);
+static void                   destroy_storage_handle(Cell name);
+static void                   show_table_state();
+static void                   init_hash_table();
+
+
+static STORAGE_HANDLE hash_table[STORAGE_HASH_SIZE];
+static xsbBool table_initted = FALSE;
+
+
+static inline STORAGE_HANDLE *get_storage_handle(Cell name)
+{
+  STORAGE_HANDLE *handle_cell;
+
+  if (!isstring(name))
+    xsb_abort("[GET_STORAGE_HANDLE] Non-string used as storage name");
+
+  if (! table_initted) init_hash_table();
+
+  handle_cell = &hash_table[storage_hash(name,STORAGE_HASH_SIZE)];
+  while (TRUE) {
+    if (handle_cell->name == name) return handle_cell;
+    if (isfree_storage_handle(handle_cell)) {
+      handle_cell->name = name;
+      handle_cell->handle= newtrie();
+      handle_cell->snapshot_number=0;
+      handle_cell->changed=FALSE;
+      return handle_cell;
+    }
+    if (handle_cell->next == NULL) {
+      handle_cell->next = (STORAGE_HANDLE *)malloc(sizeof(STORAGE_HANDLE));
+      if (handle_cell->next==NULL)
+	xsb_exit("Out of Memory: Can't allocate storage handle");
+      (handle_cell->next) -> name = (Cell)0; /* initiaize */
+      (handle_cell->next) -> next = NULL;
+      (handle_cell->next) -> prev = handle_cell;
+    }
+    handle_cell = handle_cell->next;
+  }
+
+  /* unreachable -- just to calm down the compiler */
+  return handle_cell;
+}
 
 
 STORAGE_HANDLE *storage_builtin(int builtin_number, Cell name)
@@ -59,8 +99,14 @@ STORAGE_HANDLE *storage_builtin(int builtin_number, Cell name)
     return increment_storage_snapshot(name);
   case MARK_STORAGE_CHANGED:
     return mark_storage_changed(name);
-  case DESTROY_STORAGE_HANDLE:
-    return destroy_storage_handle(name);
+  case DESTROY_STORAGE_HANDLE: {
+    destroy_storage_handle(name);
+    return NULL;
+  }
+  case SHOW_TABLE_STATE: {
+    show_table_state();
+    return NULL;
+  }
   default:
     xsb_abort("Unknown storage builtin");
     return NULL;
@@ -68,33 +114,7 @@ STORAGE_HANDLE *storage_builtin(int builtin_number, Cell name)
 }
 
 
-STORAGE_HANDLE *get_storage_handle(Cell name)
-{
-  STORAGE_HANDLE *ptr;
-
-  if (!isstring(name))
-    xsb_abort("[GET_STORAGE_HANDLE] Non-string used as storage name");
-
-  ptr = (STORAGE_HANDLE *) conget(name);
-  if (ptr != NULL)
-    return ptr;
-
-  ptr = (STORAGE_HANDLE *)malloc(sizeof(STORAGE_HANDLE));
-  if (ptr==NULL)
-    xsb_exit("Out of Memory");
-
-  conset(name,(Integer)ptr);
-
-  ptr->name = name;
-  ptr->handle= newtrie();
-  ptr->snapshot_number=0;
-  ptr->changed=FALSE;
-
-  return ptr;
-}
-
-
-STORAGE_HANDLE *increment_storage_snapshot(Cell name)
+static STORAGE_HANDLE *increment_storage_snapshot(Cell name)
 {
   STORAGE_HANDLE *ptr = get_storage_handle(name);
   ptr->snapshot_number++;
@@ -102,7 +122,7 @@ STORAGE_HANDLE *increment_storage_snapshot(Cell name)
   return ptr;
 }
 
-STORAGE_HANDLE *mark_storage_changed(Cell name)
+static STORAGE_HANDLE *mark_storage_changed(Cell name)
 {
   STORAGE_HANDLE *ptr = get_storage_handle(name);
   ptr->changed = TRUE;
@@ -111,11 +131,55 @@ STORAGE_HANDLE *mark_storage_changed(Cell name)
 
 
 
-STORAGE_HANDLE *destroy_storage_handle(Cell name)
+static void destroy_storage_handle(Cell name)
 {
   STORAGE_HANDLE *ptr = get_storage_handle(name);
-  if (ptr != NULL)
+  if (ptr->prev != NULL)
+    (ptr->prev)->next = ptr->next;
+  if (ptr->next != NULL)
+    (ptr->next)->prev = ptr->prev;
+  /* ptr->prev == NULL means this entry is in the main table, not overflow --
+     can't free it then */
+  if (ptr->prev != NULL)
     free(ptr);
-  conset(name,(Integer)NULL);
-  return NULL;
+  else {
+    ptr->name = (Cell)0;
+    ptr->next = NULL;
+  }
+}
+
+
+static void init_hash_table()
+{
+  int i;
+  table_initted = TRUE;
+  for (i=0; i < STORAGE_HASH_SIZE; i++) {
+    hash_table[i].name = (Cell)0;
+    hash_table[i].next = NULL;
+    hash_table[i].prev = NULL;
+  }
+}
+
+static void show_table_state()
+{
+  STORAGE_HANDLE *handle_cell;
+  int i;
+
+  printf("\nCell Status\tOverflow Count\n\n");
+  for (i=0; i < STORAGE_HASH_SIZE; i++) {
+    handle_cell = &hash_table[i];
+    if (isfree_storage_handle(handle_cell)) {
+      printf("   ---\t\t   ---\n");
+    } else {
+      int overflow_count=0;
+
+      printf("  taken\t\t");
+      handle_cell = hash_table[i].next;
+      while (handle_cell != NULL) {
+	overflow_count++;
+	handle_cell = handle_cell->next;
+      }
+      printf("   %d\n", overflow_count);
+    }
+  }
 }
