82a44ca36d63a52794c581c8a0a56b9e82b1fc12
xmc <xmc>
2002-08-20 18:35:52 +0000

    Tests both - NativeEngine and SubprocessEngine

--- packages/iprolog/tests/com/declarativa/interprolog/AllTests.java
+++ packages/iprolog/tests/com/declarativa/interprolog/AllTests.java
@@ -4,7 +4,7 @@ import junit.framework.*;
 import junit.extensions.*;
 public class AllTests{
 	// JUnit reloads all classes including this one, so we can not get this easily through the command line:
-	public static String startDir = "/home/miti/Summer/XSB";
+	public static String startDir = "/home/lmc/Summer02/tarfiles/Aug19/XSB";
 	public static String startCommand = startDir + "/config/i686-pc-linux-gnu/bin/xsb";
 	public static void main(String args[]) { 
 		/*startCommand=args[0];
@@ -18,7 +18,7 @@ public class AllTests{
 	public static Test suite(){
 		TestSuite suite= new TestSuite("Testing InterProlog"); 
 		suite.addTestSuite(SubprocessEngineTest.class);
-//		suite.addTest(new NativeEngineTest("testNativeEngine"));
+		suite.addTest(new NativeEngineTest("testNativeEngine"));
 		return suite;
 	}
 }
