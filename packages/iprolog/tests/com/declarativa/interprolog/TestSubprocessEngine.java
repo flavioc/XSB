3345431cf18141b45a638c638f15a99d70470837
xmc <xmc>
2002-08-20 19:45:18 +0000

    This file just tests SubprocessEngine

---
+++ packages/iprolog/tests/com/declarativa/interprolog/TestSubprocessEngine.java
@@ -0,0 +1,24 @@
+package com.declarativa.interprolog; 
+import com.xsb.interprolog.*;
+import junit.framework.*;
+import junit.extensions.*;
+public class TestSubprocessEngine{
+	// JUnit reloads all classes including this one, so we can not get this easily through the command line:
+	public static String startDir = "/home/lmc/Summer02/tarfiles/Aug19/XSB";
+	public static String startCommand = startDir + "/config/i686-pc-linux-gnu/bin/xsb";
+	public static void main(String args[]) { 
+		/*startCommand=args[0];
+		if (args.length>1) System.out.println("Invoke tests with a single argument");
+    	else*/ 
+	    System.out.println("start command : "+startCommand);
+	    junit.swingui.TestRunner.run(TestSubprocessEngine.class);
+	    
+	}
+	
+	public static Test suite(){
+		TestSuite suite= new TestSuite("Testing InterProlog"); 
+		suite.addTestSuite(SubprocessEngineTest.class);	
+		return suite;
+	}
+}
+
