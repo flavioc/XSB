10c6f6509bec3ca7c720658882ec7f97c03ebb75
saikat12 <saikat12>
2003-09-22 20:06:11 +0000

    commiting the driver manager and the prolog interface

---
+++ packages/dbdrivers/driver_manager.c
@@ -0,0 +1,585 @@
+/* File: driver_manager.c
+** Author: Saikat Mukherjee
+** Contact: saikat@cs.sunysb.edu
+
+** This file is the  middle layer for the interface to the drivers. 
+** This gets called from lib/db_interface.P and in turn calls the
+** driver specific files which are in the packages/dbdrivers/ directory.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "cinterf.h"
+#include "driver_manager.h"
+
+struct xsb_connectionHandle* CHandles[MAX_CONNECTIONS];
+struct xsb_queryHandle* QHandles[MAX_QUERIES];
+struct driver* DBdrivers[MAX_DRIVERS];
+int numDrivers, numCHandles, numQHandles;
+char* errorMesg;
+
+int initialise(void)
+{
+	numDrivers = numCHandles = numQHandles = 0;
+	errorMesg = NULL;
+	return TRUE;
+}
+
+
+int openConnection(void)
+{
+	int (*connectDriver)(struct xsb_connectionHandle*);
+	char* (*errorMesgDriver)();
+	struct xsb_connectionHandle* cHandle;
+	char *handle, *driver, *server, *database, *user, *password, *dsn;
+	int val;
+
+	handle = ptoc_string(1);
+	driver = ptoc_string(2);
+	server = ptoc_string(3);
+	if (strlen(server) == 0)
+		dsn = ptoc_string(4);
+	else
+		database = ptoc_string(4);
+	user = ptoc_string(5);
+	password = ptoc_string(6);
+
+	if (isConnectionHandle(handle) != NULL)
+	{
+		errorMesg = "XSB_DBI ERROR: repeated use of same connection handle\n";
+		return FALSE;
+	}
+
+	connectDriver = getDriverFunction(driver, CONNECT)->connectDriver;
+
+	cHandle = (struct xsb_connectionHandle *)malloc(sizeof(struct xsb_connectionHandle));
+	cHandle->handle = (char *)malloc((strlen(handle) + 1) * sizeof(char));
+	strcpy(cHandle->handle, handle);
+	cHandle->driver = (char *)malloc((strlen(driver) + 1) * sizeof(char));
+	strcpy(cHandle->driver, driver);
+	if (strlen(server) == 0)
+	{
+		cHandle->dsn = (char *)malloc((strlen(dsn) + 1) * sizeof(char));
+		strcpy(cHandle->dsn, dsn);
+		cHandle->server = NULL;
+	}
+	else
+	{
+		cHandle->server = (char *)malloc((strlen(server) + 1) * sizeof(char));
+		strcpy(cHandle->server, server);
+		cHandle->database = (char *)malloc((strlen(database) + 1) * sizeof(char));
+		strcpy(cHandle->database, database);
+		cHandle->dsn = NULL;
+	}
+	cHandle->user = (char *)malloc((strlen(user) + 1) * sizeof(char));
+	strcpy(cHandle->user, user);
+	cHandle->password = (char *)malloc((strlen(password) + 1) * sizeof(char));
+	strcpy(cHandle->password, password);
+	
+	if ((val = connectDriver(cHandle)) == SUCCESS)
+		CHandles[numCHandles++] = cHandle;
+	else
+	{
+		errorMesgDriver = getDriverFunction(cHandle->driver, ERROR_MESG)->errorMesgDriver;
+		errorMesg = errorMesgDriver();
+		free(cHandle->handle);
+		free(cHandle->driver);
+		if (strlen(server) == 0)
+			free(cHandle->dsn);
+		else
+		{
+			free(cHandle->server);
+			free(cHandle->database);
+		}
+		free(cHandle->user);
+		free(cHandle->password);
+		free(cHandle);
+		return FALSE;
+	}
+
+	return TRUE;
+}  
+
+
+int closeConnection(void)
+{
+	int (*disconnectDriver)(struct xsb_connectionHandle *);
+	int (*closeStmtDriver)(struct xsb_queryHandle *);
+	char* (*errorMesgDriver)();
+	char* handle;
+	int val, i, j;
+
+	handle = ptoc_string(1);
+
+	for (i = 0 ; i < numCHandles ; i++)
+	{
+		if (!strcmp(CHandles[i]->handle, handle))
+		{
+			disconnectDriver = getDriverFunction(CHandles[i]->driver, DISCONNECT)->disconnectDriver;
+			val = disconnectDriver(CHandles[i]);
+
+			if(val == FAILURE)
+			{
+				errorMesgDriver = getDriverFunction(CHandles[i]->driver, ERROR_MESG)->errorMesgDriver;
+				errorMesg = errorMesgDriver();
+				return FALSE;
+			}
+
+			for (j = 0 ; j < numQHandles ; j++)
+			{
+				if (!strcmp(QHandles[j]->connHandle->handle, handle))
+				{
+					closeStmtDriver = getDriverFunction(CHandles[i]->driver, ERROR_MESG)->closeStmtDriver;
+					val = closeStmtDriver(QHandles[j]);
+					if (val == FAILURE)
+					{
+						errorMesgDriver = getDriverFunction(CHandles[i]->driver, ERROR_MESG)->errorMesgDriver;
+						errorMesg = errorMesgDriver();
+						return FALSE;
+					}
+					free(QHandles[j]->handle);
+					free(QHandles[j]->query);
+					free(QHandles[j]);
+				}
+			}
+
+			free(CHandles[i]->handle);
+			free(CHandles[i]->driver);
+			if (CHandles[i]->server == NULL)
+				free(CHandles[i]->dsn);
+			else
+			{
+				free(CHandles[i]->server);
+				free(CHandles[i]->database);
+			}
+			free(CHandles[i]->user);
+			free(CHandles[i]->password);
+			free(CHandles[i]);
+			for (j = i + 1 ; j < numCHandles ; j++)
+				CHandles[j-1] = CHandles[j];
+			numCHandles--;
+			break;
+		}
+	}
+
+	
+	return TRUE;
+}
+
+
+int queryConnection(void)
+{
+	struct xsb_data** (*queryDriver)(struct xsb_queryHandle*);
+	char* (*errorMesgDriver)();
+	prolog_term returnList, sqlQueryList, element;
+	struct xsb_connectionHandle* cHandle;
+	struct xsb_queryHandle* qHandle;
+	struct xsb_data** result;
+	char *chandle, *qhandle, *sqlQuery;
+	int i, j;
+
+	chandle = ptoc_string(1);
+	qhandle = ptoc_string(2);
+	sqlQueryList = reg_term(3);
+	returnList = reg_term(4);
+
+	result = NULL;
+
+	if ((qHandle = isQueryHandle(qhandle)) != NULL)
+	{
+		if (strcmp(qHandle->connHandle->handle, chandle))
+		{
+			errorMesg = "XSB_DBI ERROR: same query handle for different connections\n";
+			return FALSE;
+		}
+		queryDriver = getDriverFunction(qHandle->connHandle->driver, QUERY)->queryDriver;
+		result = queryDriver(qHandle);
+		if (result == NULL && qHandle->state == QUERY_RETRIEVE)
+		{
+			for (i = 0 ; i < numQHandles ; i++)
+			{
+				if (!strcmp(QHandles[i]->handle, qhandle))
+				{
+					free(QHandles[i]->handle);
+					free(QHandles[i]->query);
+					free(QHandles[i]);
+					for (j = i + 1 ; j < numQHandles ; j++)
+					{
+						QHandles[j-1] = QHandles[j];
+					}
+					numQHandles--;
+					break;
+				}
+			}
+		}
+	}
+	else if ((cHandle = isConnectionHandle(chandle)) != NULL)
+	{
+		sqlQuery = buildSQLQuery(sqlQueryList);
+		qHandle = (struct xsb_queryHandle *)malloc(sizeof(struct xsb_queryHandle));
+		qHandle->handle = (char *)malloc((strlen(qhandle) + 1) * sizeof(char));
+		strcpy(qHandle->handle, qhandle);
+		qHandle->connHandle = cHandle;
+		qHandle->query = (char *)malloc((strlen(sqlQuery) + 1) * sizeof(char));
+		strcpy(qHandle->query, sqlQuery);
+		qHandle->state = QUERY_BEGIN;
+		QHandles[numQHandles++] = qHandle;
+		queryDriver = getDriverFunction(cHandle->driver, QUERY)->queryDriver;
+		result = queryDriver(qHandle);
+	}
+	else
+	{
+		errorMesg = "XSB_DBI ERROR: no such connection or query handle\n";
+		return FALSE;		
+	}
+
+	i = 0;
+	while (!is_nil(returnList))
+	{
+		element = p2p_car(returnList);
+		if (result == NULL)
+			c2p_nil(element);
+		else if (is_var(element) && result[i]->type == STRING_TYPE)
+			c2p_string(result[i]->val->str_val, element);
+		else if (is_var(element) && result[i]->type == INT_TYPE)
+			c2p_int(*(result[i]->val->i_val), element);
+		else if (is_var(element) && result[i]->type == FLOAT_TYPE)
+			c2p_float(*(result[i]->val->f_val), element);
+		returnList = p2p_cdr(returnList);
+		i++;
+	}
+
+	if (result != NULL)
+		return TRUE;
+	if ((cHandle = isConnectionHandle(chandle)) != NULL)
+	{
+		errorMesgDriver = getDriverFunction(cHandle->driver, ERROR_MESG)->errorMesgDriver;
+		errorMesg = errorMesgDriver();
+	}
+	return FALSE;
+}
+
+int prepareStatement(void)
+{
+	int (*prepareStmtDriver)(struct xsb_queryHandle*);
+	char* (*errorMesgDriver)();
+	prolog_term sqlQueryList;
+	char *chandle, *qhandle, *sqlQuery;
+	struct xsb_queryHandle* qHandle;
+	struct xsb_connectionHandle* cHandle;
+	int val;
+
+	chandle = ptoc_string(1);
+	qhandle = ptoc_string(2);
+	sqlQueryList = reg_term(3);
+	
+	if ((cHandle = isConnectionHandle(chandle)) == NULL)
+	{
+		errorMesg = "XSB_DBI ERROR: no such connection handle\n";
+		return FALSE;
+	}
+	
+	if ((qHandle = isQueryHandle(qhandle)) != NULL)
+	{
+		errorMesg = "XSB_DBI ERROR: query handle already exists\n";
+		return FALSE;
+	}
+
+	sqlQuery = buildSQLQuery(sqlQueryList);
+	qHandle = (struct xsb_queryHandle *)malloc(sizeof(struct xsb_queryHandle));
+	qHandle->connHandle = cHandle;
+	qHandle->query = (char *)malloc((strlen(sqlQuery) + 1) * sizeof(char));
+	strcpy(qHandle->query, sqlQuery);
+	qHandle->handle = (char *)malloc((strlen(qhandle) + 1) * sizeof(char));
+	strcpy(qHandle->handle, qhandle);
+	qHandle->state = QUERY_BEGIN;
+
+	prepareStmtDriver = getDriverFunction(cHandle->driver, PREPARE)->prepareStmtDriver;
+	if ((val = prepareStmtDriver(qHandle)) != -1)
+	{
+		qHandle->numParams = val;
+		QHandles[numQHandles++] = qHandle;
+	}
+	else
+	{
+		errorMesgDriver = getDriverFunction(cHandle->driver, ERROR_MESG)->errorMesgDriver;
+		errorMesg = errorMesgDriver();
+		free(qHandle->query);
+		free(qHandle);
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+int executePreparedStatement(void)
+{
+	struct xsb_data** (*executeStmtDriver)(struct xsb_data**, struct xsb_queryHandle*);
+	char* (*errorMesgDriver)();
+	struct xsb_queryHandle* qHandle;
+	struct xsb_data** bindValues;
+	struct xsb_data** result;
+	prolog_term bindList, returnList, element;
+	char *queryHandle;
+	int temp_int;
+	double temp_float;
+	int i;
+
+	queryHandle = ptoc_string(1);
+	bindList = reg_term(2);
+	returnList = reg_term(3);
+
+	if ((qHandle = isQueryHandle(queryHandle)) == NULL)
+	{
+		errorMesg = "XSB_DBI ERROR: no such query handle\n";
+		return FALSE;
+	}
+
+	if (qHandle->state == QUERY_BEGIN)
+	{
+		bindValues = (struct xsb_data **)malloc(qHandle->numParams * sizeof(struct xsb_data *));
+		for (i = 0 ; i < qHandle->numParams ; i++)
+		{
+			bindValues[i] = (struct xsb_data *)malloc(sizeof(struct xsb_data));
+			if (is_nil(bindList))
+			{
+				errorMesg = "XSB_DBI ERROR: not all paremeters supplied\n";
+				return FALSE;
+			}
+			element = p2p_car(bindList);
+			if (is_string(element))
+			{
+				bindValues[i]->type = STRING_TYPE;
+				bindValues[i]->length = strlen(p2c_string(element));
+				bindValues[i]->val = (union xsb_value *)malloc(sizeof(union xsb_value));
+				bindValues[i]->val->str_val = (char *)malloc((strlen(p2c_string(element)) + 1) * sizeof(char));
+				strcpy(bindValues[i]->val->str_val, p2c_string(element));
+			}
+			else if (is_int(element))
+			{
+				bindValues[i]->type = INT_TYPE;
+				bindValues[i]->val = (union xsb_value *)malloc(sizeof(union xsb_value));
+				bindValues[i]->val->i_val = (int *)malloc(sizeof(int));
+				temp_int = p2c_int(element);
+				bindValues[i]->val->i_val = &temp_int;
+			}
+			else if (is_float(element))
+			{
+				bindValues[i]->type = FLOAT_TYPE;
+				bindValues[i]->val = (union xsb_value *)malloc(sizeof(union xsb_value));
+				bindValues[i]->val->f_val = (double *)malloc(sizeof(double));
+				temp_float = p2c_float(element);
+				bindValues[i]->val->f_val = &temp_float;
+			}
+			else if (is_var(element))
+			{
+				errorMesg = "XSB_DBI ERROR: unbound variable in query list\n";
+				return FALSE;
+			}
+			bindList = p2p_cdr(bindList);
+		}
+	}
+	
+	executeStmtDriver = getDriverFunction(qHandle->connHandle->driver, EXEC_PREPARE)->executeStmtDriver;
+	if ((result = executeStmtDriver(bindValues, qHandle)) == NULL)
+		qHandle->state = QUERY_BEGIN;	
+
+	i = 0;
+	while (!is_nil(returnList))
+	{
+		element = p2p_car(returnList);
+		if (result == NULL)
+			c2p_nil(element);
+		else if (is_var(element) && result[i]->type == STRING_TYPE)
+			c2p_string(result[i]->val->str_val, element);
+		else if (is_var(element) && result[i]->type == INT_TYPE)
+			c2p_int(*(result[i]->val->i_val), element);
+		else if (is_var(element) && result[i]->type == FLOAT_TYPE)
+			c2p_float(*(result[i]->val->f_val), element);
+		returnList = p2p_cdr(returnList);
+		i++;
+	}
+	
+	if (result == NULL)
+	{
+		errorMesgDriver = getDriverFunction(qHandle->connHandle->driver, ERROR_MESG)->errorMesgDriver;
+		errorMesg = errorMesgDriver();
+		return FALSE;
+	}
+	
+	return TRUE;
+}
+
+int closeStatement(void)
+{
+	int (*closeStmtDriver)(struct xsb_queryHandle*);
+	char* (*errorMesgDriver)();
+	char* queryHandle;
+	char* driverName;
+	int result;
+	int i, j;
+
+	queryHandle = ptoc_string(1);
+	for (i = 0 ; i < numQHandles ; i++)
+	{
+		if (!strcmp(QHandles[i]->handle, queryHandle))
+		{
+			driverName = QHandles[i]->connHandle->driver;
+			closeStmtDriver = getDriverFunction(driverName, CLOSE_STMT)->closeStmtDriver;
+			result = closeStmtDriver(QHandles[i]);
+			if (result == FAILURE)
+			{
+				errorMesgDriver = getDriverFunction(driverName, ERROR_MESG)->errorMesgDriver;
+				errorMesg = errorMesgDriver();
+				return FALSE;
+			}
+			free(QHandles[i]->query);
+			free(QHandles[i]->handle);
+			free(QHandles[i]);
+			for (j = i + 1 ; j < numQHandles ; j++)
+				QHandles[j-1] = QHandles[j];
+			numQHandles--;
+		}
+	}
+
+	return TRUE;
+}
+
+int exception(void)
+{
+	prolog_term element;
+
+	element = reg_term(1);
+	if (is_var(element) && errorMesg != NULL)
+	{
+		c2p_string(errorMesg, element);
+		errorMesg = NULL;
+		return TRUE;
+	}
+	
+	return FALSE;
+}
+
+char* buildSQLQuery(prolog_term sqlQueryList)
+{
+	prolog_term element;
+	char* temp;
+	char* sqlQuery;
+
+	sqlQuery = (char *)malloc(100 * sizeof(char));
+	sqlQuery[0] = '\0';
+	while (!is_nil(sqlQueryList))
+	{
+		element = p2p_car(sqlQueryList);
+		if (is_string(element))		
+			strcat(sqlQuery, p2c_string(element));
+		else if (is_int(element))
+		{
+			temp = (char *)malloc(100 * sizeof(char));
+			sprintf(temp, "%d", p2c_int(element));
+			strcat(sqlQuery, temp);
+			free(temp);
+		}
+		else if (is_float(element))
+		{
+			temp = (char *)malloc(100 * sizeof(char));
+			sprintf(temp, "%f", p2c_float(element));
+			strcat(sqlQuery, temp);
+			free(temp);
+		}
+		else if (is_var(element))
+		{
+			errorMesg = "XSB_DBI ERROR: unbound variable in query list\n";
+			return NULL;
+		}
+		sqlQueryList = p2p_cdr(sqlQueryList);
+	}
+
+	return sqlQuery;
+}
+
+struct xsb_connectionHandle* isConnectionHandle(char* handle)
+{
+	int i;
+	for (i = 0 ; i < numCHandles ; i++)
+		if (!strcmp(CHandles[i]->handle, handle))
+				return CHandles[i];
+	return NULL;
+}
+
+struct xsb_queryHandle* isQueryHandle(char* handle)
+{
+	int i;
+	for (i = 0 ; i < numQHandles ; i++)
+		if (!strcmp(QHandles[i]->handle, handle))
+			return QHandles[i];
+	return NULL;
+}
+
+
+int registerXSBDriver(char* drivername, int num)
+{
+	struct driver* dr;
+	int i;
+
+	for (i = 0 ; i < numDrivers ; i++)
+	{
+		if (!strcmp(DBdrivers[i]->driver, drivername))
+		{
+			errorMesg = "XSB_DBI ERROR: driver already registered\n";
+			return -1;
+		}
+	}
+	dr = (struct driver *)malloc(sizeof(struct driver));
+	dr->driver = drivername;
+	dr->numberFunctions = num;
+	dr->functions = (struct driverFunction **)malloc(num * sizeof(struct driverFunction *));
+	for (i = 0 ; i < num ; i++)
+		dr->functions[i] = (struct driverFunction *)malloc(sizeof(struct driverFunction));
+	DBdrivers[numDrivers++] = dr;
+	return 0;
+}
+
+
+int registerXSBFunction(char* drivername, int type, union functionPtrs* func)
+{
+	int i;
+
+	for (i = 0 ; i < numDrivers ; i++)
+	{
+		if (!strcmp(DBdrivers[i]->driver, drivername))
+		{
+			DBdrivers[i]->functions[type]->functionType = type;
+			DBdrivers[i]->functions[type]->functionName = func;
+			break;
+		}
+	}
+
+	return 0;
+}
+
+
+union functionPtrs* getDriverFunction(char* drivername, int type)
+{
+	int i, j;
+
+	for (i = 0 ; i < numDrivers ; i++)
+	{
+		if (!strcmp(DBdrivers[i]->driver, drivername))
+		{
+			for (j = 0 ; j < DBdrivers[i]->numberFunctions ; j++)
+			{
+				if (DBdrivers[i]->functions[j]->functionType == type)
+					return DBdrivers[i]->functions[j]->functionName;
+			}
+			errorMesg = "XSB_DBI ERROR: no such function\n";
+			return NULL;
+		}
+	}
+	errorMesg = "XSB_DBI ERROR: no such driver\n";
+	return NULL;
+}
+
+
---
+++ packages/dbdrivers/driver_manager.h
@@ -0,0 +1,106 @@
+/* File: driver_manager.h
+** Author: Saikat Mukherjee
+** Contact: saikat@cs.sunysb.edu
+
+** Data structures, function declarations for the driver_manager
+*/
+
+
+#define MAX_CONNECTIONS 25
+#define MAX_QUERIES 25
+#define MAX_DRIVERS 25
+
+#define CONNECT 0 
+#define DISCONNECT 1
+#define QUERY 2
+#define PREPARE 3
+#define EXEC_PREPARE 4
+#define CLOSE_STMT 5
+#define ERROR_MESG 6
+
+#define INT_TYPE 1
+#define FLOAT_TYPE 2
+#define STRING_TYPE 3
+
+#define SUCCESS 0
+#define FAILURE 1
+
+#define QUERY_BEGIN 0
+#define QUERY_RETRIEVE 1
+
+
+// **** the database value data structures ****
+
+union xsb_value
+{
+	int* i_val;
+	double* f_val;
+	char* str_val;
+};
+
+struct xsb_data
+{
+	int type;
+	int length;
+	union xsb_value* val;
+};
+
+// **** the handle data structures ****
+
+struct xsb_connectionHandle
+{
+	char* handle; //key
+	char* server;
+	char* user;
+	char* database;
+	char* password;
+	char* dsn;
+	char* driver;
+};
+
+struct xsb_queryHandle
+{
+	struct xsb_connectionHandle* connHandle;
+	char* handle; // key
+	char* query;
+	int state;
+	int numParams;
+};
+
+// **** the driver data structures ****
+
+
+struct driverFunction
+{
+	int functionType;
+	union functionPtrs* functionName;	
+};
+
+struct driver
+{
+	char* driver;
+	int numberFunctions;
+	struct driverFunction** functions;
+};
+
+// **** the function ptrs and function declarations ****
+
+union functionPtrs
+{
+	int (*connectDriver)(struct xsb_connectionHandle*);
+	int (*disconnectDriver)(struct xsb_connectionHandle*);
+	struct xsb_data** (*queryDriver)(struct xsb_queryHandle*);
+	int (*prepareStmtDriver)(struct xsb_queryHandle*);
+	struct xsb_data** (*executeStmtDriver)(struct xsb_data**, struct xsb_queryHandle*);
+	int (*closeStmtDriver)(struct xsb_queryHandle*);
+	char* (*errorMesgDriver)();
+};
+
+int registerXSBDriver(char* driver, int num);
+int registerXSBFunction(char* dr, int type, union functionPtrs* func);
+struct xsb_connectionHandle* isConnectionHandle(char* handle);
+struct xsb_queryHandle* isQueryHandle(char* handle);
+char* buildSQLQuery(prolog_term sqlQueryList);
+union functionPtrs* getDriverFunction(char* driver, int type);
+
+
