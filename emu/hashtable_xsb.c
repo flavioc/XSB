f75d69415a856f9a4e30e7a0bce6391aa2ad624b
dwarren <dwarren>
2004-07-07 22:31:14 +0000

    Add a couple of includes to declare necessary symbols, as suggested by a user.

--- emu/hashtable_xsb.c
+++ emu/hashtable_xsb.c
@@ -28,6 +28,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "auxlry.h"
 #include "cell_xsb.h"
--- emu/subp.c
+++ emu/subp.c
@@ -32,6 +32,7 @@
 
 #include <stdio.h>
 #include <signal.h>
+#include <string.h>
 
 #ifdef WIN_NT
 #include <windows.h>
@@ -45,6 +46,7 @@
 #else
 #include <pthread.h>
 #include <sched.h>
+#include <unistd.h>
 #endif
 
 
