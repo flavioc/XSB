2913787fcfbf1ba4b58d3141c645d4d31c96c575
tswift <tswift>
2005-02-06 02:15:03 +0000

    Changed configure script to define a macro GUESS_TAGGING in kernal version 2.6 of linux (w. help from Harpreet).

--- build/makexsb_wind.bat
+++ build/makexsb_wind.bat
@@ -1,32 +1,32 @@
-@echo off
-REM   makexsb_wind.bat
-REM   Script for compiling XSB under Windows using VC++
-
-set XSBCONFIGdir=..\config\x86-pc-windows
-
-mkdir %XSBCONFIGdir%\saved.o
-mkdir %XSBCONFIGdir%\bin
-mkdir %XSBCONFIGdir%\lib
-
-
-@cd ..\emu
-
-REM Concatenate MSVC_mkfile.mak & MSVC.dep into emu\MSVC_mkfile.mak
-copy %XSBCONFIGdir%\MSVC_mkfile.mak+%XSBCONFIGdir%\MSVC.dep MSVC_mkfile.mak
-
-nmake /f "MSVC_mkfile.mak" %1 %2 %3 %4 %5 %6 %7
-
-del MSVC_mkfile.mak
-
-@cd ..\gpp
-nmake /f "MSVC_mkfile.mak" %1 %2 %3 %4 %5 %6 %7
-
-@cd ..\packages\dbdrivers
-nmake /f NMakefile.mak
-
-@cd ..\..\build
-
-
-REM Local Variables:
-REM coding-system-for-write:  iso-2022-7bit-dos
-REM End:
+@echo off
+REM   makexsb_wind.bat
+REM   Script for compiling XSB under Windows using VC++
+
+set XSBCONFIGdir=..\config\x86-pc-windows
+
+mkdir %XSBCONFIGdir%\saved.o
+mkdir %XSBCONFIGdir%\bin
+mkdir %XSBCONFIGdir%\lib
+
+
+@cd ..\emu
+
+REM Concatenate MSVC_mkfile.mak & MSVC.dep into emu\MSVC_mkfile.mak
+copy %XSBCONFIGdir%\MSVC_mkfile.mak+%XSBCONFIGdir%\MSVC.dep MSVC_mkfile.mak
+
+nmake /f "MSVC_mkfile.mak" %1 %2 %3 %4 %5 %6 %7
+
+del MSVC_mkfile.mak
+
+@cd ..\gpp
+nmake /f "MSVC_mkfile.mak" %1 %2 %3 %4 %5 %6 %7
+
+@cd ..\packages\dbdrivers
+nmake /f NMakefile.mak
+
+@cd ..\..\build
+
+
+REM Local Variables:
+REM coding-system-for-write:  iso-2022-7bit-dos
+REM End:
