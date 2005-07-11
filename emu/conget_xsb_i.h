e7d8e23b1e371f965e89c0dcaf2f0a671838cb3c
dwarren <dwarren>
2005-07-11 16:57:02 +0000

    Added the standard header to this file (how has it lacked it for lo these many years??)

--- emu/conget_xsb_i.h
+++ emu/conget_xsb_i.h
@@ -1,4 +1,27 @@
-
+/* File:      conget_xsb_i.h
+** Author(s): ??
+** Contact:   xsb-contact@cs.sunysb.edu
+** 
+** Copyright (C) The Research Foundation of SUNY, 1986, 1993-1999
+** Copyright (C) ECRC, Germany, 1990
+** 
+** XSB is free software; you can redistribute it and/or modify it under the
+** terms of the GNU Library General Public License as published by the Free
+** Software Foundation; either version 2 of the License, or (at your option)
+** any later version.
+** 
+** XSB is distributed in the hope that it will be useful, but WITHOUT ANY
+** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+** FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for
+** more details.
+** 
+** You should have received a copy of the GNU Library General Public License
+** along with XSB; if not, write to the Free Software Foundation,
+** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+**
+** $Id$
+** 
+*/
 
 static inline Integer conget(Cell string)
 {
