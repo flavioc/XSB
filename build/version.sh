8f79c895944a2e0545f187ef27cbe87eca8ff433
dwarren <dwarren>
2006-01-16 22:14:37 +0000

    Change version to be last release+ and name to be CVS, to indicate that this is NOT the released version.

--- build/version.sh
+++ build/version.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 xsb_major_version=2
-xsb_minor_version=7.1
+xsb_minor_version="7.1+"
 #xsb_beta_version=0
 #xsb_codename="Skol"        # for Version 2.1
 #xsb_codename="Tsingtao"    # for Version 2.2
@@ -9,7 +9,8 @@ xsb_minor_version=7.1
 #xsb_codename="Bavaria"     # the beer from Holland
 #xsb_codename="Okocim"      # from Poland! ;)
 #xsb_codename="Duff"        # Simpsons
-xsb_codename="Kinryo"       # 2.7 Jonah's friend's uncle's sake'
+#xsb_codename="Kinryo"       # 2.7 Jonah's friend's uncle's sake'
+xsb_codename="CVS"	    # CVS
 
 # Format: YYYY-MM-DD or YYYY.MM.DD or YYYY/MM/DD
 # With this, XSB should become Y2K compliant :-)
