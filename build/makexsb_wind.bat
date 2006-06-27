5673645630664874d4e52fe5a2917351d027a6f3
harpreet_singh <harpreet_singh>
2006-06-27 13:24:58 +0000

    quieter.

--- build/makexsb_wind.bat
+++ build/makexsb_wind.bat
@@ -4,35 +4,34 @@ REM   Script for compiling XSB under Windows using VC++
 
 set XSBCONFIGdir=..\config\x86-pc-windows
 
-mkdir %XSBCONFIGdir%\saved.o
-mkdir %XSBCONFIGdir%\bin
-mkdir %XSBCONFIGdir%\lib
-
+IF NOT EXIST %XSBCONFIGdir%\saved.o MKDIR %XSBCONFIGdir%\saved.o
+IF NOT EXIST %XSBCONFIGdir%\bin mkdir %XSBCONFIGdir%\bin
+IF NOT EXIST %XSBCONFIGdir%\lib mkdir %XSBCONFIGdir%\lib
 
 @cd ..\emu
 
 REM Concatenate MSVC_mkfile.mak & MSVC.dep into emu\MSVC_mkfile.mak
-copy %XSBCONFIGdir%\MSVC_mkfile.mak+%XSBCONFIGdir%\MSVC.dep MSVC_mkfile.mak
+@copy %XSBCONFIGdir%\MSVC_mkfile.mak+%XSBCONFIGdir%\MSVC.dep MSVC_mkfile.mak
 
-nmake /f "MSVC_mkfile.mak" %1 %2 %3 %4 %5 %6 %7
+@nmake /nologo /f "MSVC_mkfile.mak" %1 %2 %3 %4 %5 %6 %7
 
-del MSVC_mkfile.mak
+@del MSVC_mkfile.mak
 
 @cd ..\gpp
-nmake /f "MSVC_mkfile.mak" %1 %2 %3 %4 %5 %6 %7
+@nmake /nologo /s /f "MSVC_mkfile.mak" %1 %2 %3 %4 %5 %6 %7
 
 @cd ..\packages
 
 @cd dbdrivers
-nmake /f NMakefile.mak
+@nmake /nologo /s /f NMakefile.mak
 @cd ..
 
 @cd sgml\cc
-nmake /f NMakefile.mak
+@nmake /nologo /f NMakefile.mak
 @cd ..\..
 
 @cd xpath\cc
-nmake /f NMakefile.mak
+@nmake /nologo /f NMakefile.mak
 @cd ..\..
 
 @cd ..\build
