02c9b53f7558b98aeff5d81d75b6504d50a8ba20
harpreet_singh <harpreet_singh>
2006-06-27 13:22:34 +0000

    Increased makedepend line lenght so all deps are on one line. Use unix2dos instead of sed for converting file to win format.

--- build/MSVC.sh
+++ build/MSVC.sh
@@ -9,17 +9,16 @@ cd ../emu
 
 configdir=../config/x86-pc-windows
 
-# -w1000 tells makedepend to create long line, 1 dependency per source file: 
+# -w2000 tells makedepend to create long line, 1 dependency per source file:
 # we don't know how MSVC's NMAKE handles multiple dependencies per source file
 # -Y tells not to bother with system include dirs:
 # they aren't important as dependencies
-# Grep is here so that the warnings about system include files 
-# won't be displayed. 
-makedepend -w1000 -f ../build/.makedepend.tmp -o.obj -p@@@ -Y -- -I$configdir -- *c 2>&1 \
+# Grep is here so that the warnings about system include files
+# won't be displayed.
+makedepend -w2000 -f ../build/.makedepend.tmp -o.obj -p@@@ -Y -- -I$configdir -- *c 2>&1 \
 	| grep -v "cannot find include" | grep -v "not in"
 
 # Convert Unix Makefile dependencies to NMAKE format, add ^M at the end
-cat ../build/.makedepend.tmp \
-        | sed  -f ../build/MSVC.sed \
-        | sed 's/$//'  > ../build/MSVC.dep
+cat ../build/.makedepend.tmp | sed  -f ../build/MSVC.sed | unix2dos --unix2dos > ../build/MSVC.dep
+
 
