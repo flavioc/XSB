d3059ccb2d05f030fe899006450c89cb60dba02a
harpreet_singh <harpreet_singh>
2006-07-26 15:22:12 +0000

    Bump version to 3.0.

--- build/version.sh
+++ build/version.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-xsb_major_version=2
-xsb_minor_version="7.1+"
+xsb_major_version=3
+xsb_minor_version=0.0
 #xsb_beta_version=0
 #xsb_codename="Skol"        # for Version 2.1
 #xsb_codename="Tsingtao"    # for Version 2.2
@@ -9,9 +9,10 @@ xsb_minor_version="7.1+"
 #xsb_codename="Bavaria"     # the beer from Holland
 #xsb_codename="Okocim"      # from Poland! ;)
 #xsb_codename="Duff"        # Simpsons
-#xsb_codename="Kinryo"       # 2.7 Jonah's friend's uncle's sake'
-xsb_codename="CVS"	    # CVS
+#xsb_codename="Kinryo"      # 2.7 Jonah's friend's uncle's sake'
+xsb_codename="Sagres"       # version 3.0.0
+#xsb_codename="CVS"	    	# CVS
 
 # Format: YYYY-MM-DD or YYYY.MM.DD or YYYY/MM/DD
 # With this, XSB should become Y2K compliant :-)
-xsb_release_date=2005-03-05
+xsb_release_date=2005-07-29	
