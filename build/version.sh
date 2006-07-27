e3f182b7849a7d48f131915328c44619630ad945
harpreet_singh <harpreet_singh>
2006-07-27 16:38:22 +0000

    Changed version and codename for CVS builds.

--- build/version.sh
+++ build/version.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 xsb_major_version=3
-xsb_minor_version=0
+xsb_minor_version="0+"
 #xsb_beta_version=0
 #xsb_codename="Skol"        # for Version 2.1
 #xsb_codename="Tsingtao"    # for Version 2.2
@@ -10,9 +10,9 @@ xsb_minor_version=0
 #xsb_codename="Okocim"      # from Poland! ;)
 #xsb_codename="Duff"        # Simpsons
 #xsb_codename="Kinryo"      # 2.7 Jonah's friend's uncle's sake'
-xsb_codename="Sagres"       # version 3.0.0
-#xsb_codename="CVS"	    	# CVS
+#xsb_codename="Sagres"       # version 3.0.0
+xsb_codename="CVS"	    	# CVS
 
 # Format: YYYY-MM-DD or YYYY.MM.DD or YYYY/MM/DD
 # With this, XSB should become Y2K compliant :-)
-xsb_release_date=2005-07-29	
+xsb_release_date=2006-07-29	
