6b6203794bb52c7c9797f8cf30bbd83f1bcc01c8
ruim <ruim>
2006-11-08 17:06:33 +0000

    solved bug in CONC_COMPL involving completion stack expansion taken out some unneeded ifded CONC_COMPL

--- emu/memory_xsb.c
+++ emu/memory_xsb.c
@@ -507,6 +507,12 @@ void complstack_realloc (CTXTdeclc long new_size) {
        csf_ptr < (ComplStackFrame)new_bottom;
        csf_ptr++) {
     subg_ptr = compl_subgoal_ptr(csf_ptr);
+#ifdef CONC_COMPL
+    /* In CONC_COMPL there might be completion stack frames pointing to
+       subgoal frames owned by other threads which in turn point
+       to the other thread's completion stack */
+    if( subg_tid(subg_ptr) == th->tid )
+#endif
     subg_compl_stack_ptr(subg_ptr) =
       (CPtr)((byte *)subg_compl_stack_ptr(subg_ptr) + bottom_offset);
   } 
