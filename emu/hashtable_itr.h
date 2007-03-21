fe2dec3395b71eebe4373a0781cb89ce73c434c1
dwarren <dwarren>
2007-03-21 15:16:56 +0000

    Comment out an unneeded include (for inline, which is not being used any more.)  Quiets a complaining compiler.

--- emu/hashtable_itr.h
+++ emu/hashtable_itr.h
@@ -3,7 +3,7 @@
 #ifndef __HASHTABLE_ITR_CWC22__
 #define __HASHTABLE_ITR_CWC22__
 #include "hashtable.h"
-#include "hashtable_private.h" /* needed to enable inlining */
+/** #include "hashtable_private.h"  needed to enable inlining */
 
 /*****************************************************************************/
 /* This struct is only concrete here to allow the inlining of two of the
