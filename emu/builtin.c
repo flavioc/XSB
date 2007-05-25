5e372fce46c3763129d84257d6711c2f8d991606
dwarren <dwarren>
2007-05-25 18:11:29 +0000

    Added coercion to quiet the MSVC compiler.

--- emu/builtin.c
+++ emu/builtin.c
@@ -2988,7 +2988,7 @@ int print_xsb_backtrace(CTXTdecl) {
     tmp_ereg = ereg;
     tmp_cpreg = cpreg;
     if (!tmp_cpreg) instruction = *(tmp_cpreg-2*sizeof(Cell));
-    else instruction = fail_inst;
+    else instruction = (unsigned char)fail_inst;
     while (tmp_cpreg && (instruction == call || instruction == trymeorelse) && 
 	   (backtrace_length++ < MAX_BACKTRACE_LENGTH)) {
       if (instruction == call) {
