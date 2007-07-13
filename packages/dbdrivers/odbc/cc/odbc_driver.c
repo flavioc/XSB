f0a45705f4b16189d69511dd25165c37deb4166d
kifer <kifer>
2007-07-13 18:30:58 +0000

    fug fixes in configure.in and related

--- packages/dbdrivers/odbc/cc/odbc_driver.c
+++ packages/dbdrivers/odbc/cc/odbc_driver.c
@@ -183,7 +183,7 @@ struct xsb_data** driverODBC_query(struct xsb_queryHandle* handle)
     }
     handle->numResultCols = query->resultmeta->numCols;
     if (query->resultmeta->numCols == 0) {
-      printf("num=%d\n",numQueries);
+      //printf("num=%d\n",numQueries);
       if (odbcHandles[numQueries]==NULL)
 	odbcQueries[numQueries++] = query;
       return NULL;
