3e3dd148b2e9d9a9005742f7a980d68ccbbd8d61
tswift <tswift>
2007-09-27 17:43:33 +0000

    Removing old interprolog stuff, which never made it as a package.

--- packages/xasp/xasppkg.c
+++ packages/xasp/xasppkg.c
@@ -31,23 +31,30 @@
 #endif
 #include "api.h"
 #include "atomrule.h"
+#include "xsb_config.h"b
 
 // These variables should not be global in the MT-engine.
 // They will soon be moved to the th_context structure so that
 // each thread can have its own Smodels instance.
+#ifdef MULTI_THREAD
+#include "context.h"
+#include "xasp.h"
+Atom **globatoms;
+#else
 Smodels *smodels;
 Api *api;
 Atom **atoms;
 int curatom,totatoms; /* current atom, used during creation */
+#endif
 
-extern "C" void init(void)
+extern "C" void init(CTXTdecl)
 {
-  smodels = new Smodels;
+  smodels = (new Smodels);
 
-  api = new Api(&(smodels->program));
+  api = new Api(&(((Smodels *) (th->_smodels))->program));
 }
 
-extern "C" void numberAtoms(int nAtoms)
+extern "C" void numberAtoms(CTXTdeclc int nAtoms)
 {
   int i;
 
@@ -59,151 +66,178 @@ extern "C" void numberAtoms(int nAtoms)
   totatoms=nAtoms;
 }
 
-extern "C" void atomName(char *name)
+/*
+extern "C" void atomName(CTXTdeclc char *name)
 { 
-  api->set_name(atoms[curatom],name);
+  Atom ** loc_atoms = atoms;
+  int loc_curatom = curatom;
+  api->set_name(globatoms[loc_curatom],name);
   curatom++;
 }
+*/
 
-extern "C" void beginBasicRule(void)
+extern "C" void beginBasicRule(CTXTdecl)
 {
   api->begin_rule(BASICRULE);
 }
 
-extern "C" void beginChoiceRule(void)
+extern "C" void beginChoiceRule(CTXTdecl)
 {
   api->begin_rule(CHOICERULE);
 }
 
-extern "C" void beginConstraintRule(void)
+extern "C" void beginConstraintRule(CTXTdecl)
 {
   api->begin_rule(CONSTRAINTRULE);
 }
 
-extern "C" void beginWeightRule(void)
+extern "C" void beginWeightRule(CTXTdecl)
 {
   api->begin_rule(WEIGHTRULE);
 }
 
-extern "C" void addHead(int atomNum)
+extern "C" void addHead(CTXTdeclc int atomNum)
 {
   api->add_head(atoms[atomNum-1]);
 }
 
-extern "C" void addWPosBody(int atomNum,Weight weight)
+extern "C" void addWPosBody(CTXTdeclc int atomNum,Weight weight)
 {
   api->add_body(atoms[atomNum-1],true,weight);
 }
 
-extern "C" void addPosBody(int atomNum)
+extern "C" void addPosBody(CTXTdeclc int atomNum)
 {
   api->add_body(atoms[atomNum-1],true);
 }
 
-extern "C" void addWNegBody(int atomNum,Weight weight)
+extern "C" void addWNegBody(CTXTdeclc int atomNum,Weight weight)
 {
   api->add_body(atoms[atomNum-1],false,weight);
 }
 
-extern "C" void addNegBody(int atomNum)
+extern "C" void addNegBody(CTXTdeclc int atomNum)
 {
   api->add_body(atoms[atomNum-1],false);
 }
 
-extern "C" void endRule(void)
+extern "C" void endRule(CTXTdecl)
 {
   api->end_rule();
 }
 
-extern "C" void commitRules(void)
+extern "C" void commitRules(CTXTdecl)
 {
   api->done();
   smodels->init();
 }
 
-extern "C" void printProgram(void)
+extern "C" void printProgram(CTXTdecl)
 {
   smodels->program.print();
 }
 
-extern "C" int existsModel(void)
+extern "C" int existsModel(CTXTdecl)
 {
+  printf("exists Model called\n");
   return smodels->model();
 }
 
-extern "C" void printAnswer(void)
+extern "C" void printAnswer(CTXTdecl)
 {
   smodels->printAnswer();
 }
 
-extern "C" void close(void)
+extern "C" void close(CTXTdecl)
 {
   delete(api);
   delete(smodels);
   free(atoms);
 }
 
-extern "C" int checkAtom(int atom)
+extern "C" int checkAtom(CTXTdeclc int atom)
 {
   return atoms[atom-1]->Bpos;
 }
 
-extern "C" void setPosCompute(int atom)
+extern "C" void setPosCompute(CTXTdeclc int atom)
 {
   api->set_compute(atoms[atom-1],true);
 }
 
-extern "C" void setNegCompute(int atom)
+extern "C" void setNegCompute(CTXTdeclc int atom)
 {
   api->set_compute(atoms[atom-1],false);
 }
 
-extern "C" void resetPosCompute(int atom)
+extern "C" void resetPosCompute(CTXTdeclc int atom)
 {
   api->reset_compute(atoms[atom-1],true);
 }
 
-extern "C" void resetNegCompute(int atom)
+extern "C" void resetNegCompute(CTXTdeclc int atom)
 {
   api->reset_compute(atoms[atom-1],false);
 }
 
-extern "C" void remember(void)
+extern "C" void remember(CTXTdecl )
 {
   api->remember();
 }
 
-extern "C" void forget(void)
+extern "C" void forget(CTXTdecl )
 {
   api->forget();
 }
 
-extern "C" void setBody(long val)
+extern "C" void setBody(CTXTdeclc long val)
 {
   api->set_atleast_body(val);
 }
 
-extern "C" void setWeight(long val)
+extern "C" void setWeight(CTXTdeclc long val)
 {
   api->set_atleast_weight(val);
 }
 
-extern "C" void setHead(long val)
+extern "C" void setHead(CTXTdeclc long val)
 {
   api->set_atleast_head(val);
 }
 
-extern "C" void wellfounded(void)
+extern "C" void wellfounded(CTXTdecl)
 {
   smodels->wellfounded();
 }
 
-extern "C" int testPos(int atom)
+extern "C" int testPos(CTXTdeclc int atom)
 {
   return smodels->testPos(atoms[atom-1]);
 }
 
-extern "C" int testNeg(int atom)
+extern "C" int testNeg(CTXTdeclc int atom)
 {
   return smodels->testNeg(atoms[atom-1]);
+}
+
+extern "C" void dummy1(int atom)
+{
+  printf("%s\n",atom);
+}
+
+extern "C" void dummy2(void)
+{
+  printf("%s\n","foo");
+}
+
+extern "C" int dummy3(int atom)
+{
+  printf("%s\n",atom);
+  return 1;
+}
+
+extern "C" int dummy4(void)
+{
+  printf("%s\n","foo");
+  return 1;
 }
