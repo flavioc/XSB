c0b33cd84741be8e48945ed78ce105477e127354
tswift <tswift>
2009-02-27 00:24:07 +0000

    Kopi Lewak

--- build/version.sh
+++ build/version.sh
@@ -1,19 +1,21 @@
+
 #! /bin/sh
 
 xsb_major_version=3
-xsb_minor_version="1"
-#xsb_beta_version=0
+xsb_minor_version="2-pre"
+#xsb_beta_version=pre"
 #xsb_codename="Skol"        # for Version 2.1
 #xsb_codename="Tsingtao"    # for Version 2.2
 #xsb_codename="Zombie"      # Version 2.3
 #xsb_codename="Bavaria"     # the beer from Holland
 #xsb_codename="Okocim"      # from Poland! ;)
 #xsb_codename="Duff"        # Simpsons
-#xsb_codename="Kinryo"      # 2.7 Jonah's friend's uncle's sake'
+#xsb_codename="Kinryo"      # 2.7 Jonah's friend's uncle's sake
 #xsb_codename="Sagres"      # version 3.0, Portuguese Beer, Rui's favorite
-xsb_codename="Incognito"    # version 3.1 Portuguese Syrah intro'd to Terry by Antonio Porto
+#xsb_codename="Incognito"    # version 3.1 Portuguese Syrah intro'd to Terry by Antonio Porto
+xsb_codename="Kopi Lewak"    # Version 3.2 Civit coffee
 
 # Format: YYYY-MM-DD or YYYY.MM.DD or YYYY/MM/DD
 # With this, XSB should become Y2K compliant :-)
 #xsb_release_date=2006-08-07
-xsb_release_date=2007-08-10
+xsb_release_date=2009-02-26
