f9d6f0ede761ae41b3ff1949ff4aa1a551188a7e
tswift <tswift>
2009-03-29 18:37:32 +0000

    Adding 64-bit configurations.

--- admin/configure_release.sh
+++ admin/configure_release.sh
@@ -20,6 +20,18 @@ makexsb ;
  configure --enable-batched  > /tmp/config ; 
  makexsb --config-tag=btc ;
 
+ echo "--------------- 64-bit configure -------------------------"
+
+ configure --with-bits64 ;
+ makexsb --config-tag=bits64;
+
+echo "--------------- mt-64 configure -------------------------"
+
+ cd ../XSB/build
+
+ configure --with-bits64 --enable-mt ;
+ makexsb --config-tag=bits64-mt ;
+
 #--------------------------------------------------------
  
 rm ../config/*/saved.o/*.o ; 
